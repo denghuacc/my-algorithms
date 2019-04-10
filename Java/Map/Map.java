8bc587a10ef58ade5ab8607bfc7f3af65e234f72
Hale Deng <haledeng88@gmail.com>
2019-04-10 23:40:52 +0800

    add Map structrue

---
+++ Java/Map/Map.java
@@ -0,0 +1,19 @@
+/**
+ * Map 接口
+ */
+public interface Map<K, V> {
+
+  void add(K key, V value);
+
+  V remove(K key);
+
+  boolean contains(K key);
+
+  V get(K key);
+
+  void set(K key, V newValue);
+
+  int getSize();
+
+  boolean isEmpty();
+}
