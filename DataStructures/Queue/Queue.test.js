cbeefc37a41306aac55eb0edffc12f740755183e
Hale Deng <haledeng88@gmail.com>
2020-01-31 20:52:48 +0800

    refactor: update test code

--- DataStructures/Queue/Queue.test.js
+++ DataStructures/Queue/Queue.test.js
@@ -2,12 +2,15 @@ const Queue = require('./Queue')
 
 let queue = new Queue()
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   queue.enqueue(i)
   console.log(queue.toString())
-
-  if (i % 3 === 2) {
-    queue.dequeue()
-    console.log(queue.toString())
-  }
 }
+
+console.log(queue.toString()) // Queue: front [ 0, 1, 2, 3, 4 ]
+
+queue.dequeue()
+console.log(queue.toString()) // Queue: front [ 0, 1, 2, 3 ]
+
+console.log('first element:', queue.getFront()) // 1
+console.log('element size:', queue.getSize()) // 4
