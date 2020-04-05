2e69165bf08b6fec00846b0ca041283f573e7e58
Hale Deng <haledeng88@gmail.com>
2020-04-05 22:37:40 +0800

    chore: add solution

---
+++ Leetcode/3.无重复字符的最长子串.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (27.86%)
+ * Likes:    3405
+ * Dislikes: 0
+ * Total Accepted:    415.5K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let ret = 0,
+    i = 0,
+    temp = []
+
+  while (i < s.length) {
+    if (temp.indexOf(s[i]) === -1) {
+      temp.push(s[i])
+    } else {
+      temp.shift()
+      continue
+    }
+
+    ret = Math.max(ret, temp.length)
+    i++
+  }
+
+  return ret
+}
+// @lc code=end
