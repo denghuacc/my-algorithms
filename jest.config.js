d1c1eb3032e4d50e4f088c40d26c156a1e4f9b90
Hale Deng <haledeng88@gmail.com>
2020-04-10 18:18:45 +0800

    chore: add jest config file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rootDir: __dirname,
+  testRegex: '/.*\\.(spec)\\.(js)$',
+  moduleFileExtensions: ['js']
+  // coveragePathIgnorePatterns: ['/node_modules/'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 90,
+  //     functions: 95,
+  //     lines: 95,
+  //     statements: 95
+  //   }
+  // },
+  // collectCoverageFrom: ['**/*.{js}']
+}
