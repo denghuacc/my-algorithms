45f213ef8280560f5d80edc1c5519f01535e4179
Hale Deng <haledeng88@gmail.com>
2020-04-13 11:11:43 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   rootDir: __dirname,
-  testRegex: '/.*\\.(spec)\\.(js)$',
+  testMatch: ['<rootDir>/**/__tests__/**/*.spec.js'],
   moduleFileExtensions: ['js']
-  // coveragePathIgnorePatterns: ['/node_modules/'],
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 90,
-  //     functions: 95,
-  //     lines: 95,
-  //     statements: 95
-  //   }
-  // },
-  // collectCoverageFrom: ['**/*.{js}']
 }
