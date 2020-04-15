33a534f4a776a354b20bf90bc5b3848893f2f1e2
Hale Deng <haledeng88@gmail.com>
2020-04-15 21:17:10 +0800

    chore: update typescript test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  preset: 'ts-jest',
   rootDir: __dirname,
-  testMatch: ['<rootDir>/**/__tests__/**/*.spec.js'],
-  moduleFileExtensions: ['js']
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/__tests__/**/*spec.[t]s'],
+  moduleFileExtensions: ['ts', 'js'],
+  watchPathIgnorePatterns: ['/node_modules/', '/.git/']
+  // coverageDirectory: 'coverage',
+  // coverageReporters: ['html', 'lcov', 'text'],
+  // collectCoverageFrom: [
+  //   '/algorithms/**/*.{js,ts}',
+  //   '/data-structures/**/*.{js,ts}'
+  // ],
+  // coveragePathIgnorePatterns: ['/node_modules/', '/leetcode/']
 }
