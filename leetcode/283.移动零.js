346b45f60dcc528c237f43336519a7d234afd496
Hale Deng <haledeng88@gmail.com>
2020-04-17 22:41:56 +0800

    chore: add solution

---
+++ leetcode/283.移动零.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ *
+ * https://leetcode-cn.com/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (52.26%)
+ * Likes:    561
+ * Dislikes: 0
+ * Total Accepted:    134.3K
+ * Total Submissions: 222.1K
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ * 说明:
+ *
+ *
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let point = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      ;[nums[i], nums[point]] = [nums[point], nums[i]]
+      point++
+    }
+  }
+  return nums
+}
+
+var moveZeroes = function (nums) {
+  let point = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (!nums[i]) {
+      nums[point++] = nums[i]
+    }
+  }
+  for (let i = point; i < nums.length; i++) {
+    nums[i] = 0
+  }
+}
+// @lc code=end
