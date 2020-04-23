65bb14f9c7467b7435e6f1362cdf85c4d04bfc95
Hale Deng <haledeng88@gmail.com>
2020-04-24 00:26:31 +0800

    feat: add shuffle code

---
+++ algorithms/shuffle/fisher-yates.ts
@@ -0,0 +1,12 @@
+// 洗牌算法（随机算法）
+
+import { swap } from '../util'
+
+export function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1))
+    swap(array, i, randomIndex)
+  }
+
+  return array
+}
