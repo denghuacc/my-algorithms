8fce11f0ba66123e19fcc86d357a53fe76705e2d
Hale Deng <haledeng88@gmail.com>
2020-04-29 23:42:24 +0800

    chore: add solution

---
+++ leetcode/linked-list/86.分隔链表.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ *
+ * https://leetcode-cn.com/problems/partition-list/description/
+ *
+ * algorithms
+ * Medium (44.13%)
+ * Likes:    186
+ * Dislikes: 0
+ * Total Accepted:    34.3K
+ * Total Submissions: 59.9K
+ * Testcase Example:  '[1,4,3,2,5,2]\n3'
+ *
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ *
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ *
+ * 示例:
+ *
+ * 输入: head = 1->4->3->2->5->2, x = 3
+ * 输出: 1->2->2->4->3->5
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  let beforeHead = new ListNode(-1)
+  let before = beforeHead
+  let afterHead = new ListNode(-1)
+  let after = afterHead
+
+  while (head != null) {
+    if (head.val < x) {
+      before.next = head
+      before = before.next
+    } else {
+      after.next = head
+      after = after.next
+    }
+
+    head = head.next
+  }
+
+  after.next = null // 最后节点
+  before.next = afterHead.next // 连接链表
+
+  return beforeHead.next
+}
+// @lc code=end
