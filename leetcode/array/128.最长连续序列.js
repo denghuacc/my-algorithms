6625eeae921561d49c92562580c9438961c8b727
Hale Deng <haledeng88@gmail.com>
2020-05-05 22:42:34 +0800

    chore: add solution

---
+++ leetcode/array/128.最长连续序列.js
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=javascript
+ *
+ * [128] 最长连续序列
+ *
+ * https://leetcode-cn.com/problems/longest-consecutive-sequence/description/
+ *
+ * algorithms
+ * Hard (40.90%)
+ * Likes:    311
+ * Dislikes: 0
+ * Total Accepted:    36.5K
+ * Total Submissions: 75.1K
+ * Testcase Example:  '[100,4,200,1,3,2]'
+ *
+ * 给定一个未排序的整数数组，找出最长连续序列的长度。
+ *
+ * 要求算法的时间复杂度为 O(n)。
+ *
+ * 示例:
+ *
+ * 输入: [100, 4, 200, 1, 3, 2]
+ * 输出: 4
+ * 解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * brute JS 超时
+ */
+// var longestConsecutive = function (nums) {
+//   let longest = 0
+//   for (const num of nums) {
+//     let curNum = num
+//     let curLongest = 1
+
+//     while (nums.indexOf(curNum + 1) > -1) {
+//       curNum += 1
+//       curLongest += 1
+//     }
+
+//     longest = Math.max(longest, curLongest)
+//   }
+
+//   return longest
+// }
+
+// sort
+var longestConsecutive = function (nums) {
+  if (nums.length === 0) return 0
+
+  nums = nums.sort((a, b) => a - b)
+
+  let longest = 1
+  let curLongest = 1
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      if (nums[i] === nums[i - 1] + 1) {
+        curLongest += 1
+      } else {
+        longest = Math.max(longest, curLongest)
+        curLongest = 1
+      }
+    }
+  }
+
+  return Math.max(longest, curLongest)
+}
+
+// hash
+var longestConsecutive = function (nums) {
+  const set = new Set()
+
+  for (const num of nums) {
+    set.add(num)
+  }
+
+  let longest = 0
+
+  for (const num of set.values()) {
+    if (!set.has(num - 1)) {
+      let curNum = num
+      let curLongest = 1
+
+      while (set.has(curNum + 1)) {
+        curNum += 1
+        curLongest += 1
+      }
+
+      longest = Math.max(longest, curLongest)
+    }
+  }
+
+  return longest
+}
+// @lc code=end
