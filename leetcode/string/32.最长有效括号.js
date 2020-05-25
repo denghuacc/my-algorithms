603cb2170728055b476242c0d6c77ec72e6edd13
Hale Deng <haledeng88@gmail.com>
2020-05-25 21:48:06 +0800

    chore: add solution

---
+++ leetcode/string/32.最长有效括号.js
@@ -0,0 +1,147 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=javascript
+ *
+ * [32] 最长有效括号
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (25.16%)
+ * Likes:    648
+ * Dislikes: 0
+ * Total Accepted:    57.9K
+ * Total Submissions: 190K
+ * Testcase Example:  '"(()"'
+ *
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ *
+ * 示例 1:
+ *
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ * 暴力超时
+ */
+var longestValidParentheses = function (s) {
+  let maxLen = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 2; j <= s.length; j += 2) {
+      if (isValid(s.substring(i, j))) {
+        maxLen = Math.max(maxLen, j - i);
+      }
+    }
+  }
+
+  return maxLen;
+
+  function isValid(s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === "(") {
+        stack.push("(");
+      } else if (stack.length && stack[stack.length - 1] === "(") {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+
+    return stack.length === 0;
+  }
+};
+
+// dp error
+var longestValidParentheses = function (s) {
+  let maxLen = 0;
+  const dp = [];
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === ")") {
+      if (s[i - 1] === "(") {
+        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+      } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] === "(") {
+        dp[i] =
+          dp[i - 1] + ((i - dp[i - 1] >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2);
+      }
+      maxLen = Math.max(maxLen, dp[i]);
+    }
+  }
+
+  return maxLen;
+};
+
+// stack
+var longestValidParentheses = function (s) {
+  let maxLen = 0;
+  const stack = [];
+  stack.push(-1);
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else {
+      stack.pop();
+      if (!stack.length) {
+        stack.push(i);
+      } else {
+        maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+      }
+    }
+  }
+  return maxLen;
+};
+
+// double pointer
+var longestValidParentheses = function (s) {
+  let left = 0;
+  let right = 0;
+  let maxLen = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      left++;
+    } else {
+      right++;
+    }
+
+    if (left === right) {
+      maxLen = Math.max(maxLen, right * 2);
+    } else if (right >= left) {
+      left = right = 0;
+    }
+  }
+
+  left = right = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === "(") {
+      left++;
+    } else {
+      right++;
+    }
+
+    if (left === right) {
+      maxLen = Math.max(maxLen, left * 2);
+    } else if (left >= right) {
+      left = right = 0;
+    }
+  }
+  return maxLen;
+};
+// @lc code=end
