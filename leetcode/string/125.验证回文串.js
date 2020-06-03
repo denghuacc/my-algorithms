f3306ad853fc5bc49bd9bc4e62c9e96927097a4d
Hale Deng <haledeng88@gmail.com>
2020-06-03 23:08:23 +0800

    chore: add solution

---
+++ leetcode/string/125.验证回文串.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=javascript
+ *
+ * [125] 验证回文串
+ *
+ * https://leetcode-cn.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (37.71%)
+ * Likes:    195
+ * Dislikes: 0
+ * Total Accepted:    106.9K
+ * Total Submissions: 243.5K
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ *
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ * 示例 1:
+ *
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: "race a car"
+ * 输出: false
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    if (s[l] !== s[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+
+  return true;
+};
+// @lc code=end
