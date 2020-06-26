7d7ae01543a01f9c70901fd581b1959f0b775600
Hale Deng <haledeng88@gmail.com>
2020-06-26 15:20:17 +0800

    chore: add solution

---
+++ leetcode/backtracking/51.n皇后.js
@@ -0,0 +1,138 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=javascript
+ *
+ * [51] N皇后
+ *
+ * https://leetcode-cn.com/problems/n-queens/description/
+ *
+ * algorithms
+ * Hard (58.04%)
+ * Likes:    436
+ * Dislikes: 0
+ * Total Accepted:    45.9K
+ * Total Submissions: 65.5K
+ * Testcase Example:  '4'
+ *
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ *
+ *
+ * 上图为 8 皇后问题的一种解法。
+ *
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ *
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * 示例:
+ *
+ * 输入: 4
+ * 输出: [
+ * ⁠[".Q..",  // 解法 1
+ * ⁠ "...Q",
+ * ⁠ "Q...",
+ * ⁠ "..Q."],
+ *
+ * ⁠["..Q.",  // 解法 2
+ * ⁠ "Q...",
+ * ⁠ "...Q",
+ * ⁠ ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ *
+ * 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一到七步，可进可退。（引用自
+ * 百度百科 - 皇后 ）
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+  const ret = [];
+  dfs(n, [], ret);
+  return ret;
+
+  function dfs(n, tmp, ret) {
+    if (tmp.length === n) {
+      ret.push(
+        tmp.map((i) => {
+          let arr = Array(n).fill(".");
+          arr.splice(i, 1, "Q");
+          return arr.join("");
+        })
+      );
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (isValid(tmp, i)) {
+        tmp.push(i);
+        dfs(n, tmp, ret);
+        tmp.pop();
+      }
+    }
+  }
+
+  function isValid(arr, idx) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+      const item = arr[i];
+      if (item === idx || i - item === len - idx || i + item === len + idx) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+// backtrack
+var solveNQueens = function (n) {
+  const obj = {};
+  const add = [];
+  const sub = [];
+  const ret = [];
+  backtrack(0);
+  return ret;
+
+  function backtrack(row) {
+    if (row === n) {
+      const arr = [];
+      for (const key in obj) {
+        arr.push("Q".padStart(obj[key] + 1, ".").padEnd(n, "."));
+      }
+      ret.push(arr);
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (
+        Object.values(obj).indexOf(i) !== -1 ||
+        add.indexOf(row + i) !== -1 ||
+        sub.indexOf(row - i) !== -1
+      ) {
+        continue;
+      }
+
+      obj[row] = i;
+      add.push(row + i);
+      sub.push(row - i);
+
+      backtrack(row + 1);
+
+      obj[row] = -1;
+      add.pop();
+      sub.pop();
+    }
+  }
+};
+// @lc code=end
---
+++ leetcode/backtracking/52.n皇后-ii.js
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=javascript
+ *
+ * [52] N皇后 II
+ *
+ * https://leetcode-cn.com/problems/n-queens-ii/description/
+ *
+ * algorithms
+ * Hard (69.54%)
+ * Likes:    126
+ * Dislikes: 0
+ * Total Accepted:    25.5K
+ * Total Submissions: 32.4K
+ * Testcase Example:  '4'
+ *
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ *
+ *
+ * 上图为 8 皇后问题的一种解法。
+ *
+ * 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+ *
+ * 示例:
+ *
+ * 输入: 4
+ * 输出: 2
+ * 解释: 4 皇后问题存在如下两个不同的解法。
+ * [
+ * [".Q..",  // 解法 1
+ * "...Q",
+ * "Q...",
+ * "..Q."],
+ *
+ * ["..Q.",  // 解法 2
+ * "Q...",
+ * "...Q",
+ * ".Q.."]
+ * ]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ *
+ * 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一或七步，可进可退。（引用自
+ * 百度百科 - 皇后 ）
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function (n) {
+  const obj = {};
+  const add = [];
+  const sub = [];
+  let ret = 0;
+  backtrack(0);
+  return ret;
+
+  function backtrack(row) {
+    if (row === n) {
+      ret++;
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (
+        Object.values(obj).indexOf(i) !== -1 ||
+        add.indexOf(row + i) !== -1 ||
+        sub.indexOf(row - i) !== -1
+      ) {
+        continue;
+      }
+
+      obj[row] = i;
+      add.push(row + i);
+      sub.push(row - i);
+
+      backtrack(row + 1);
+
+      obj[row] = -1;
+      add.pop();
+      sub.pop();
+    }
+  }
+};
+// @lc code=end
