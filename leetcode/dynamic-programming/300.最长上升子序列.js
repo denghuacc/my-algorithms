202759b28e3e8163b23068df0334f515b7fc3492
Hale Deng <haledeng88@gmail.com>
2020-07-01 10:53:01 +0800

    chore: add solution

--- leetcode/dynamic-programming/300.最长上升子序列.js
+++ leetcode/dynamic-programming/300.最长上升子序列.js
@@ -39,57 +39,97 @@
  * dp
  */
 var lengthOfLIS = function (nums) {
-  if (nums.length === 0) return 0
+  if (nums.length === 0) return 0;
 
-  const dp = new Array(nums.length)
-  dp[0] = 1
-  let len = 1
+  const dp = Array(nums.length);
+  dp[0] = 1;
+  let len = 1;
 
   for (let i = 1; i < dp.length; i++) {
-    let maxVal = 0
+    let maxVal = 0;
     for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        maxVal = Math.max(maxVal, dp[j])
+      if (nums[j] < nums[i]) {
+        maxVal = Math.max(maxVal, dp[j]);
       }
     }
-    dp[i] = maxVal + 1
-    len = Math.max(len, dp[i])
+    dp[i] = maxVal + 1;
+    len = Math.max(len, dp[i]);
   }
 
-  return len
-}
+  return len;
+};
+
+// dp2
+var lengthOfLIS = function (nums) {
+  const dp = Array(nums.length).fill(1); // base case
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  let ret = 0;
+  for (let i = 0; i < dp.length; i++) {
+    ret = Math.max(ret, dp[i]);
+  }
+  return ret;
+};
 
 // Greedy
 var lengthOfLIS = function (nums) {
-  let len = 1
-  let n = nums.length
-  if (n === 0) return 0
+  let len = 1;
+  let n = nums.length;
+  if (n === 0) return 0;
 
-  const arr = new Array(n + 1)
-  arr[len] = nums[0]
+  const arr = new Array(n + 1);
+  arr[len] = nums[0];
 
   for (let i = 1; i < n; ++i) {
     if (nums[i] > arr[len]) {
-      arr[++len] = nums[i]
+      arr[++len] = nums[i];
     } else {
-      let l = 1
-      let r = len
-      let pos = 0
+      let l = 1;
+      let r = len;
+      let pos = 0;
 
       while (l <= r) {
-        let mid = Math.floor((l + r) / 2)
+        let mid = Math.floor((l + r) / 2);
         if (arr[mid] < nums[i]) {
-          pos = mid
-          l = mid + 1
+          pos = mid;
+          l = mid + 1;
         } else {
-          r = mid - 1
+          r = mid - 1;
         }
       }
 
-      arr[pos + 1] = nums[i]
+      arr[pos + 1] = nums[i];
     }
   }
 
-  return len
-}
+  return len;
+};
+
+// binary search
+var lengthOfLIS = function (nums) {
+  const top = Array(nums.length);
+  let piles = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let poker = nums[i];
+
+    let left = 0;
+    let right = piles;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (top[mid] > poker) right = mid;
+      else if (top[mid] < poker) left = mid + 1;
+      else right = mid;
+    }
+    if (left === piles) piles++;
+    top[left] = poker;
+  }
+  return piles;
+};
 // @lc code=end
