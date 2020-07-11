e33cc65683ab141f067e93f30509ece3a9300d0f
Hale Deng <haledeng88@gmail.com>
2020-07-11 09:58:54 +0800

    chore: add solution

---
+++ leetcode/tree/315.计算右侧小于当前元素的个数.ts
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=315 lang=typescript
+ *
+ * [315] 计算右侧小于当前元素的个数
+ *
+ * https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/description/
+ *
+ * algorithms
+ * Hard (37.55%)
+ * Likes:    267
+ * Dislikes: 0
+ * Total Accepted:    18.7K
+ * Total Submissions: 48.8K
+ * Testcase Example:  '[5,2,6,1]'
+ *
+ * 给定一个整数数组 nums，按要求返回一个新数组 counts。数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于
+ * nums[i] 的元素的数量。
+ *
+ * 示例:
+ *
+ * 输入: [5,2,6,1]
+ * 输出: [2,1,1,0]
+ * 解释:
+ * 5 的右侧有 2 个更小的元素 (2 和 1).
+ * 2 的右侧仅有 1 个更小的元素 (1).
+ * 6 的右侧有 1 个更小的元素 (1).
+ * 1 的右侧有 0 个更小的元素.
+ *
+ *
+ */
+
+export {};
+
+// @lc code=start
+// BST
+class TNode {
+  val: number;
+  left: TNode | null;
+  right: TNode | null;
+  count: number;
+
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.count = 0; // 左子树节点数量
+  }
+}
+
+var countSmaller = function (nums: number[]): number[] {
+  const n = nums.length;
+  let root: TNode | null = null;
+  const ret = new Array(n).fill(0);
+  for (let i = n - 1; i >= 0; i--) {
+    root = insertNode(root, nums[i], ret, i)!;
+  }
+  return ret;
+
+  function insertNode(
+    root: TNode | null,
+    val: number,
+    ret: number[],
+    retIndx: number
+  ): TNode | null {
+    if (!root) root = new TNode(val);
+    //  小于当前节点值则放入左子树
+    else if (val <= root.val) {
+      root.count += 1; // root 的左侧节点数量值 +1
+      root.left = insertNode(root.left, val, ret, retIndx);
+    } else if (val > root.val) {
+      // 大于当前节点值则放入右子树
+      ret[retIndx] += root.count + 1; // 计算题目所求的结果
+      root.right = insertNode(root.right, val, ret, retIndx);
+    }
+
+    return root;
+  }
+};
+// @lc code=end
