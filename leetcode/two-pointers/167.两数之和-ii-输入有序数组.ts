5174f5c41b1923cee6ad2b385c04ef75f9bdcb2a
Hale Deng <haledeng88@gmail.com>
2020-07-20 09:04:59 +0800

    chore: move and clock in

--- leetcode/two-pointers/167.两数之和-ii-输入有序数组.ts
+++ leetcode/two-pointers/167.两数之和-ii-输入有序数组.ts
@@ -45,22 +45,41 @@ var twoSum = function (numbers: number[], target: number): number[] {
   return [-1, -1];
 };
 
-// binary search
+// two pointers
 var twoSum = function (numbers: number[], target: number): number[] {
-  let low = 0;
-  let high = numbers.length - 1;
+  let left = 0;
+  let right = numbers.length - 1;
 
-  while (low < high) {
-    const sum = numbers[low] + numbers[high];
-    if (sum === target) {
-      return [low + 1, high + 1];
-    } else if (sum < target) {
-      ++low;
-    } else {
-      --high;
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+    if (target === sum) {
+      return [left + 1, right + 1];
+    } else if (target > sum) {
+      left++;
+    } else if (target < sum) {
+      right--;
     }
   }
 
+  return [-1, -1];
+};
+
+// binary search
+var twoSum = function (numbers: number[], target: number): number[] {
+  for (let i = 0; i < numbers.length; i++) {
+    let left = i + 1;
+    let right = numbers.length - 1;
+    while (left <= right) {
+      let mid = left + Math.floor((right - left) / 2);
+      if (target === numbers[i] + numbers[mid]) {
+        return [i + 1, mid + 1];
+      } else if (target > numbers[i] + numbers[mid]) {
+        left = mid + 1;
+      } else if (target < numbers[i] + numbers[mid]) {
+        right = mid - 1;
+      }
+    }
+  }
   return [-1, -1];
 };
 // @lc code=end
