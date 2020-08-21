2d4ca99c2a6533877cf07e44a92e23f830f3c2b5
Hale Deng <haledeng88@gmail.com>
2020-08-21 14:56:31 +0800

    chore: add solution

--- leetcode/tree/104.二叉树的最大深度.ts
+++ leetcode/tree/104.二叉树的最大深度.ts
@@ -45,6 +45,17 @@ class TreeNode {
   }
 }
 
+// 解题思路 递归
+// 1. root == null 深度 0
+// 2. 分别求出 left 和 right 的深度
+// 3. 取最大值 + 1 为 root 的最大深度
+
+// 解题思路 迭代
+// 1. root == null 深度 0
+// 2. 创建 queue， root 和它的深度入列
+// 3. root 出列，如果有左右子节点入列，如此循环
+// 4. 每次循环深度 + 1
+
 // @lc code=start
 // recursive
 var maxDepth = function (root: TreeNode | null): number {
@@ -54,5 +65,26 @@ var maxDepth = function (root: TreeNode | null): number {
     const right = maxDepth(root.right);
     return Math.max(left, right) + 1;
   }
+};
+
+// bfs iterative
+var maxDepth = function (root: TreeNode | null): number {
+  if (!root) return 0;
+  const queue: TreeNode[] = [];
+  queue.push(root);
+  let max = 0;
+
+  while (queue.length) {
+    let size = queue.length;
+    while (size > 0) {
+      const node = queue.shift()!;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      size--;
+    }
+    max++; // 每次循环深度 + 1
+  }
+
+  return max;
 };
 // @lc code=end
