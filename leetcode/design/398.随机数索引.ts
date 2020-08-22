00c92eb2d9970b43f59e8936b608c0ca682295a2
Hale Deng <haledeng88@gmail.com>
2020-08-22 23:47:33 +0800

    chore: add solution

---
+++ leetcode/design/398.随机数索引.ts
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=398 lang=typescript
+ *
+ * [398] 随机数索引
+ *
+ * https://leetcode-cn.com/problems/random-pick-index/description/
+ *
+ * algorithms
+ * Medium (49.64%)
+ * Likes:    65
+ * Dislikes: 0
+ * Total Accepted:    5.9K
+ * Total Submissions: 9.5K
+ * Testcase Example:  '["Solution","pick"]\n[[[1,2,3,3,3]],[3]]'
+ *
+ * 给定一个可能含有重复元素的整数数组，要求随机输出给定的数字的索引。 您可以假设给定的数字一定存在于数组中。
+ *
+ * 注意：
+ * 数组大小可能非常大。 使用太多额外空间的解决方案将不会通过测试。
+ *
+ * 示例:
+ *
+ *
+ * int[] nums = new int[] {1,2,3,3,3};
+ * Solution solution = new Solution(nums);
+ *
+ * // pick(3) 应该返回索引 2,3 或者 4。每个索引的返回概率应该相等。
+ * solution.pick(3);
+ *
+ * // pick(1) 应该返回 0。因为只有nums[0]等于1。
+ * solution.pick(1);
+ *
+ *
+ */
+
+// @lc code=start
+// design
+class Solution {
+  array: number[];
+
+  constructor(nums: number[]) {
+    this.array = nums;
+  }
+
+  pick(target: number): number {
+    const indexArray: number[] = [];
+
+    // 收集 target 的全部索引
+    for (let i = 0; i < this.array.length; i++) {
+      if (target === this.array[i]) {
+        indexArray.push(i);
+      }
+    }
+
+    const random = this.createRandom(indexArray.length);
+
+    return indexArray[random];
+  }
+
+  // random range [0, n)
+  private createRandom(n: number) {
+    return Math.floor(Math.random() * Math.floor(n));
+  }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.pick(target)
+ */
+// @lc code=end
