231785df772484b4815f984d62d96dfdaf8dbb96
Hale Deng <haledeng88@gmail.com>
2020-09-02 16:57:29 +0800

    refactor: update code and comments

--- algorithms/backtracting/rat-in-a-maze.ts
+++ algorithms/backtracting/rat-in-a-maze.ts
@@ -1,20 +1,18 @@
 // 迷宫老鼠问题
+// 假设我们有一个大小为 N × N 的矩阵，矩阵的每个位置是一个方块。每个位置（或块）可以
+// 是空闲的（值为 1）或是被阻挡的（值为 0），如下图所示，其中 S 是起点， D 是终点。
+// 矩阵就是迷宫，“老鼠”的目标是从位置[0][0]开始并移动到[n-1][n-1]（终点）。
+// 老鼠可以在垂直或水平方向上任何未被阻挡的位置间移动。
 
 export function ratInAMaze(maze: number[][]) {
-  const solution: number[][] = [];
+  const n = maze.length;
+  const m = maze[0].length;
+  const solution: number[][] = Array.from(new Array(n), () =>
+    new Array(m).fill(0)
+  );
 
-  for (let i = 0; i < maze.length; i++) {
-    solution[i] = [];
-    for (let j = 0; j < maze[i].length; j++) {
-      solution[i][j] = 0;
-    }
-  }
-
-  if (findPath(maze, 0, 0, solution)) {
-    return solution;
-  } else {
-    return "NO PATH FOUND";
-  }
+  if (findPath(maze, 0, 0, solution)) return solution;
+  else return "NO PATH FOUND";
 }
 
 function findPath(
@@ -24,26 +22,29 @@ function findPath(
   solution: number[][]
 ) {
   const n = maze.length;
+  const m = maze[0].length;
 
-  if (x === n - 1 && y === n - 1) {
+  if (x === n - 1 && y === m - 1) {
     solution[x][y] = 1;
     return true;
   }
 
   if (isSafe(maze, x, y) === true) {
-    solution[x][y] = 1;
+    solution[x][y] = 1; // ! 标记为 1
     if (findPath(maze, x + 1, y, solution)) return true;
     if (findPath(maze, x, y + 1, solution)) return true;
-    solution[x][y] = 0;
+    solution[x][y] = 0; // ! 退回为 0
     return false;
   }
 
   return false;
 }
 
+// ! 块为 1 时空闲（安全）
 function isSafe(maze: number[][], x: number, y: number) {
   const n = maze.length;
-  if (x >= 0 && y >= 0 && x < n && y < n && maze[x][y] !== 0) {
+  const m = maze[0].length;
+  if (x >= 0 && y >= 0 && x < n && y < m && maze[x][y] !== 0) {
     return true;
   }
   return false;
--- algorithms/backtracting/sudoku-solver.ts
+++ algorithms/backtracting/sudoku-solver.ts
@@ -1,19 +1,19 @@
 // 数独解题器
+// 数独是一个非常有趣的解谜游戏，也是史上最流行的游戏之一。
+// 目标是用数字 1～9 填满一个 9 × 9 的矩阵，要求每行和每列都由这九个数字构成。
+// 矩阵还包含了小方块（3 × 3 矩阵），它们同样需要分别用这九个数字填满。
 
 const UNASSIGNED = 0;
 
 export function sudokuSolver(grid: number[][]) {
-  if (solveSudoku(grid) === true) {
-    return grid;
-  } else {
-    return "NO SOLUTION EXISTS!";
-  }
+  if (solveSudoku(grid) === true) return grid;
+  else return "NO SOLUTION EXISTS!";
 }
 
 function solveSudoku(grid: number[][]) {
   let row = 0;
   let col = 0;
-  let checkBlankSpaces = false;
+  let checkBlankSpaces = false; // ! 检查是否有空白，即值为 0
 
   for (row = 0; row < grid.length; row++) {
     for (col = 0; col < grid[row].length; col++) {
@@ -22,48 +22,48 @@ function solveSudoku(grid: number[][]) {
         break;
       }
     }
-    if (checkBlankSpaces === true) {
-      break;
-    }
+    if (checkBlankSpaces === true) break;
   }
 
-  if (checkBlankSpaces === false) {
-    return true;
-  }
+  if (checkBlankSpaces === false) return true; // ! 没有空白， 直接返回 true
 
   for (let num = 1; num <= 9; num++) {
     if (isSafe(grid, row, col, num)) {
-      grid[row][col] = num;
-
-      if (solveSudoku(grid)) {
-        return true;
-      }
-
-      grid[row][col] = UNASSIGNED;
+      grid[row][col] = num; // ! 设置该数字
+      if (solveSudoku(grid)) return true;
+      grid[row][col] = UNASSIGNED; // ! 回退重置为 0
     }
   }
 
   return false;
 }
 
+// ! 检查 grid[row][col] 位置 num 是否合适 -> 即其它位置原先不存在该数字
+function isSafe(grid: number[][], row: number, col: number, num: number) {
+  return (
+    !usedInRow(grid, row, num) &&
+    !usedInCol(grid, col, num) &&
+    !usedInBox(grid, row - (row % 3), col - (col % 3), num)
+  );
+}
+
+// ! 检查数字是否在行中存在
 function usedInRow(grid: number[][], row: number, num: number) {
   for (let col = 0; col < grid.length; col++) {
-    if (grid[row][col] === num) {
-      return true;
-    }
+    if (grid[row][col] === num) return true;
   }
   return false;
 }
 
+// ! 检查数字是否在列中存在
 function usedInCol(grid: number[][], col: number, num: number) {
   for (let row = 0; row < grid.length; row++) {
-    if (grid[row][col] === num) {
-      return true;
-    }
+    if (grid[row][col] === num) return true;
   }
   return false;
 }
 
+// ! 检查数字是否在 3 * 3 盒子中存在
 function usedInBox(
   grid: number[][],
   boxStartRow: number,
@@ -78,12 +78,4 @@ function usedInBox(
     }
   }
   return false;
-}
-
-function isSafe(grid: number[][], row: number, col: number, num: number) {
-  return (
-    !usedInRow(grid, row, num) &&
-    !usedInCol(grid, col, num) &&
-    !usedInBox(grid, row - (row % 3), col - (col % 3), num)
-  );
 }
