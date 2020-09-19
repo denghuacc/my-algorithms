0b4ba4a6fd44a15d1061db5b6466497dd0c27e5a
Hale Deng <haledeng88@gmail.com>
2020-09-19 10:37:48 +0800

    chore: add solution

---
+++ leetcode/tree/173.二叉搜索树迭代器.ts
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=173 lang=typescript
+ *
+ * [173] 二叉搜索树迭代器
+ *
+ * https://leetcode-cn.com/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (74.35%)
+ * Likes:    249
+ * Dislikes: 0
+ * Total Accepted:    29.6K
+ * Total Submissions: 39.7K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+  '[[[7,3,15,null,null,9,20]],[null],[null],[null],[null],[null],[null],[null],[null],[null]]'
+ *
+ * 实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+ * 
+ * 调用 next() 将返回二叉搜索树中的下一个最小的数。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 
+ * 
+ * BSTIterator iterator = new BSTIterator(root);
+ * iterator.next();    // 返回 3
+ * iterator.next();    // 返回 7
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 9
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 15
+ * iterator.hasNext(); // 返回 true
+ * iterator.next();    // 返回 20
+ * iterator.hasNext(); // 返回 false
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+ * 
+ * 
+ */
+
+export {};
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// @lc code=start
+class BSTIterator {
+  idx: number;
+  items: number[];
+  constructor(root: TreeNode | null) {
+    this.idx = 0;
+    this.items = [];
+    // inorder gen ascending order element items
+    inorder(root, this.items);
+    function inorder(node: TreeNode | null, items: number[]) {
+      if (node) {
+        if (node.left) inorder(node.left, items);
+        items.push(node.val);
+        if (node.right) inorder(node.right, items);
+      }
+    }
+  }
+
+  next(): number {
+    return this.items[this.idx++];
+  }
+
+  hasNext(): boolean {
+    if (this.idx === this.items.length) {
+      return false;
+    }
+    return true;
+  }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+// @lc code=end
---
+++ leetcode/tree/199.二叉树的右视图.ts
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=typescript
+ *
+ * [199] 二叉树的右视图
+ *
+ * https://leetcode-cn.com/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (64.25%)
+ * Likes:    318
+ * Dislikes: 0
+ * Total Accepted:    66.7K
+ * Total Submissions: 103.9K
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ *
+ * ⁠  1            <---
+ * ⁠/   \
+ * 2     3         <---
+ * ⁠\     \
+ * ⁠ 5     4       <---
+ *
+ *
+ */
+
+export {};
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// @lc code=start
+// dfs
+var rightSideView = function (root: TreeNode | null): number[] {
+  const ret: number[] = [];
+  if (!root) return ret;
+  const levels: number[][] = [];
+  dfs(root, 0);
+  for (const level of levels) {
+    ret.push(level.pop()!); // push the last one of level
+  }
+
+  return ret;
+
+  function dfs(node: TreeNode, depth: number) {
+    if (levels.length === depth) levels.push([]);
+    levels[depth].push(node.val);
+    if (node.left) dfs(node.left, depth + 1);
+    if (node.right) dfs(node.right, depth + 1);
+  }
+};
+
+// dfs2
+var rightSideView = function (root: TreeNode | null): number[] {
+  const ret: number[] = [];
+  if (!root) return ret;
+  let maxDepth = -1;
+  dfs(root, 0);
+  return ret;
+
+  function dfs(node: TreeNode, depth: number) {
+    if (depth > maxDepth) {
+      maxDepth = depth;
+      ret.push(node.val);
+    }
+
+    // prioritize process right child
+    // and the next loop can get its val
+    if (node.right) dfs(node.right, depth + 1);
+    if (node.left) dfs(node.left, depth + 1);
+  }
+};
+
+// bfs
+var rightSideView = function (root: TreeNode | null): number[] {
+  const ret: number[] = [];
+  if (!root) return ret;
+  const queue: TreeNode[] = [];
+  queue.push(root);
+
+  while (queue.length) {
+    const size = queue.length;
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift()!;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      // push last node val in this loop
+      // pushing children to queue in this loop not disturb the size
+      if (i === size - 1) {
+        ret.push(node.val);
+      }
+    }
+  }
+
+  return ret;
+};
+// @lc code=end
