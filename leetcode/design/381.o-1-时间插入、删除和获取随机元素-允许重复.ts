1e82abd31af6baac5c3385a71cf7566cc92c4fa0
Hale Deng <haledeng88@gmail.com>
2020-10-31 17:45:17 +0800

    chore: clock in

---
+++ leetcode/design/381.o-1-时间插入、删除和获取随机元素-允许重复.ts
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=381 lang=typescript
+ *
+ * [381] O(1) 时间插入、删除和获取随机元素 - 允许重复
+ *
+ * https://leetcode-cn.com/problems/insert-delete-getrandom-o1-duplicates-allowed/description/
+ *
+ * algorithms
+ * Hard (38.07%)
+ * Likes:    126
+ * Dislikes: 0
+ * Total Accepted:    8.3K
+ * Total Submissions: 19.6K
+ * Testcase Example:  '["RandomizedCollection","insert","insert","insert","getRandom","remove","getRandom"]\n' +
+  '[[],[1],[1],[2],[],[1],[]]'
+ *
+ * 设计一个支持在平均 时间复杂度 O(1) 下， 执行以下操作的数据结构。
+ * 
+ * 注意: 允许出现重复元素。
+ * 
+ * 
+ * insert(val)：向集合中插入元素 val。
+ * remove(val)：当 val 存在时，从集合中移除一个 val。
+ * getRandom：从现有集合中随机获取一个元素。每个元素被返回的概率应该与其在集合中的数量呈线性相关。
+ * 
+ * 
+ * 示例:
+ * 
+ * // 初始化一个空的集合。
+ * RandomizedCollection collection = new RandomizedCollection();
+ * 
+ * // 向集合中插入 1 。返回 true 表示集合不包含 1 。
+ * collection.insert(1);
+ * 
+ * // 向集合中插入另一个 1 。返回 false 表示集合包含 1 。集合现在包含 [1,1] 。
+ * collection.insert(1);
+ * 
+ * // 向集合中插入 2 ，返回 true 。集合现在包含 [1,1,2] 。
+ * collection.insert(2);
+ * 
+ * // getRandom 应当有 2/3 的概率返回 1 ，1/3 的概率返回 2 。
+ * collection.getRandom();
+ * 
+ * // 从集合中删除 1 ，返回 true 。集合现在包含 [1,2] 。
+ * collection.remove(1);
+ * 
+ * // getRandom 应有相同概率返回 1 和 2 。
+ * collection.getRandom();
+ * 
+ * 
+ */
+
+// @lc code=start
+class RandomizedCollection {
+  items: number[];
+  idx: Map<number, Set<number>>; // Map<val, [index]>
+
+  constructor() {
+    this.items = [];
+    this.idx = new Map();
+  }
+
+  insert(val: number): boolean {
+    this.items.push(val);
+    const set = this.idx.has(val) ? this.idx.get(val)! : new Set<number>();
+    set.add(this.items.length - 1);
+    this.idx.set(val, set);
+    return set.size === 1;
+  }
+
+  remove(val: number): boolean {
+    if (!this.idx.has(val)) return false;
+    let i!: number;
+
+    // val has assign defined index
+    // so it can remove one of its indexes
+    for (const num of this.idx.get(val)!) {
+      if (!i) {
+        i = num;
+        break;
+      }
+    }
+    // swap val and lastItem and remove val via pop() for O(1)
+    const lastItem = this.items[this.items.length - 1];
+    this.items[i] = lastItem;
+    this.idx.get(val)?.delete(i);
+    this.idx.get(lastItem)?.delete(this.items.length - 1);
+    if (i < this.items.length - 1) {
+      this.idx.get(lastItem)?.add(i);
+    }
+    if (this.idx.get(val)?.size === 0) {
+      this.idx.delete(val);
+    }
+    this.items.pop();
+
+    return true;
+  }
+
+  getRandom(): number {
+    const size = this.items.length;
+    const randomIndex = this.createRandom(size);
+    return this.items[randomIndex];
+  }
+
+  // random range [0, n)
+  private createRandom(n: number) {
+    return Math.floor(Math.random() * Math.floor(n));
+  }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * var obj = new RandomizedCollection()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+// @lc code=end
