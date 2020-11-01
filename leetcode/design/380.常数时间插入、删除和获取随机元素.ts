04c9c1047ea93711a2227d5a6291847f40cd03a1
Hale Deng <haledeng88@gmail.com>
2020-11-01 21:06:00 +0800

    chore: add solution

---
+++ leetcode/design/380.常数时间插入、删除和获取随机元素.ts
@@ -0,0 +1,113 @@
+/*
+ * @lc app=leetcode.cn id=380 lang=typescript
+ *
+ * [380] 常数时间插入、删除和获取随机元素
+ *
+ * https://leetcode-cn.com/problems/insert-delete-getrandom-o1/description/
+ *
+ * algorithms
+ * Medium (49.26%)
+ * Likes:    222
+ * Dislikes: 0
+ * Total Accepted:    18.7K
+ * Total Submissions: 38K
+ * Testcase Example:  '["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]\n' +
+  '[[],[1],[2],[2],[],[1],[2],[]]'
+ *
+ * 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构。
+ * 
+ * 
+ * insert(val)：当元素 val 不存在时，向集合中插入该项。
+ * remove(val)：元素 val 存在时，从集合中移除该项。
+ * getRandom：随机返回现有集合中的一项。每个元素应该有相同的概率被返回。
+ * 
+ * 
+ * 示例 :
+ * 
+ * 
+ * // 初始化一个空的集合。
+ * RandomizedSet randomSet = new RandomizedSet();
+ * 
+ * // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+ * randomSet.insert(1);
+ * 
+ * // 返回 false ，表示集合中不存在 2 。
+ * randomSet.remove(2);
+ * 
+ * // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+ * randomSet.insert(2);
+ * 
+ * // getRandom 应随机返回 1 或 2 。
+ * randomSet.getRandom();
+ * 
+ * // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+ * randomSet.remove(1);
+ * 
+ * // 2 已在集合中，所以返回 false 。
+ * randomSet.insert(2);
+ * 
+ * // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+ * randomSet.getRandom();
+ * 
+ * 
+ */
+
+import { time } from "console";
+
+// @lc code=start
+class RandomizedSet {
+  items: number[];
+  ids: Map<number, number>;
+
+  constructor() {
+    this.items = [];
+    this.ids = new Map();
+  }
+
+  insert(val: number): boolean {
+    if (this.ids.has(val)) return false;
+    this.items.push(val);
+    this.ids.set(val, this.items.length - 1);
+    return true;
+  }
+
+  remove(val: number): boolean {
+    if (this.ids.has(val)) {
+      const index = this.ids.get(val)!;
+      const lastIndex = this.items.length - 1;
+      const lastItem = this.items[lastIndex];
+
+      // swap val and lastItem in items
+      this.items[lastIndex] = val;
+      this.items[index] = lastItem;
+
+      // update index of lastItem in ids
+      this.ids.set(lastItem, index);
+
+      // remove val in ids and items
+      this.ids.delete(val);
+      this.items.pop();
+      return true;
+    }
+    return false;
+  }
+
+  getRandom(): number {
+    const randomIndex = this.createRandom(this.items.length);
+    return this.items[randomIndex];
+  }
+
+  // random range [0, n)
+  private createRandom(n: number) {
+    return Math.floor(Math.random() * Math.floor(n));
+  }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+// @lc code=end
---
+++ leetcode/dynamic-programming/139.单词拆分.ts
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=typescript
+ *
+ * [139] 单词拆分
+ *
+ * https://leetcode-cn.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (47.97%)
+ * Likes:    726
+ * Dislikes: 0
+ * Total Accepted:    97.5K
+ * Total Submissions: 203.2K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ *
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ *
+ *
+ */
+
+// @lc code=start
+// dp
+var wordBreak = function (s: string, wordDict: string[]): boolean {
+  const n = s.length;
+  const wordDictSet: Set<string> = new Set(wordDict);
+
+  // dp[i] -> s[0 ... i-1] 是否可拆分
+  const dp: boolean[] = new Array(n + 1).fill(false);
+  dp[0] = true;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDictSet.has(s.substr(j, i - j))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[n];
+};
+// @lc code=end
