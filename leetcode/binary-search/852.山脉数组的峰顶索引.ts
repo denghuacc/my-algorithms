0b92f81ac413cbb4a13733c0c91cba346bacc578
Hale Deng <haledeng88@gmail.com>
2020-11-03 21:33:42 +0800

    chore: add solution

---
+++ leetcode/binary-search/852.山脉数组的峰顶索引.ts
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=852 lang=typescript
+ *
+ * [852] 山脉数组的峰顶索引
+ *
+ * https://leetcode-cn.com/problems/peak-index-in-a-mountain-array/description/
+ *
+ * algorithms
+ * Easy (70.53%)
+ * Likes:    121
+ * Dislikes: 0
+ * Total Accepted:    32.1K
+ * Total Submissions: 45.5K
+ * Testcase Example:  '[0,1,0]'
+ *
+ * 我们把符合下列属性的数组 A 称作山脉：
+ *
+ *
+ * A.length >= 3
+ * 存在 0 < i < A.length - 1 使得A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... >
+ * A[A.length - 1]
+ *
+ *
+ * 给定一个确定为山脉的数组，返回任何满足 A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... >
+ * A[A.length - 1] 的 i 的值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[0,1,0]
+ * 输出：1
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：[0,2,1,0]
+ * 输出：1
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 3 <= A.length <= 10000
+ * 0 <= A[i] <= 10^6
+ * A 是如上定义的山脉
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+// traverse
+var peakIndexInMountainArray = function (arr: number[]): number {
+  const n = arr.length;
+  let index = -1;
+  for (let i = 1; i < n - 1; i++) {
+    if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+      index = i;
+    }
+  }
+  return index;
+};
+
+// traverse 2
+var peakIndexInMountainArray = function (arr: number[]): number {
+  let i = 0;
+  while (arr[i] < arr[i + 1]) i++;
+  return i;
+};
+
+// binary search
+var peakIndexInMountainArray = function (arr: number[]): number {
+  const n = arr.length;
+  let i = 0;
+  let j = n - 1;
+
+  while (i < j) {
+    let mid = i + Math.floor((j - i) / 2);
+    if (arr[mid] < arr[mid + 1]) {
+      i = mid + 1;
+    } else {
+      j = mid;
+    }
+  }
+
+  return i;
+};
+// @lc code=end
