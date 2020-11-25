84d411f4c71bdda96bf34779203c1d7a2ede73aa
Hale Deng <haledeng88@gmail.com>
2020-11-25 10:06:10 +0800

    chore: clock in

---
+++ leetcode/sort/1370.上升下降字符串.ts
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode.cn id=1370 lang=typescript
+ *
+ * [1370] 上升下降字符串
+ *
+ * https://leetcode-cn.com/problems/increasing-decreasing-string/description/
+ *
+ * algorithms
+ * Easy (74.18%)
+ * Likes:    33
+ * Dislikes: 0
+ * Total Accepted:    15.4K
+ * Total Submissions: 20.2K
+ * Testcase Example:  '"aaaabbbbcccc"'
+ *
+ * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+ *
+ *
+ * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+ * 重复步骤 2 ，直到你没法从 s 中选择字符。
+ * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+ * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+ * 重复步骤 5 ，直到你没法从 s 中选择字符。
+ * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+ *
+ *
+ * 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+ *
+ * 请你返回将 s 中字符重新排序后的 结果字符串 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "aaaabbbbcccc"
+ * 输出："abccbaabccba"
+ * 解释：第一轮的步骤 1，2，3 后，结果字符串为 result = "abc"
+ * 第一轮的步骤 4，5，6 后，结果字符串为 result = "abccba"
+ * 第一轮结束，现在 s = "aabbcc" ，我们再次回到步骤 1
+ * 第二轮的步骤 1，2，3 后，结果字符串为 result = "abccbaabc"
+ * 第二轮的步骤 4，5，6 后，结果字符串为 result = "abccbaabccba"
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：s = "rat"
+ * 输出："art"
+ * 解释：单词 "rat" 在上述算法重排序以后变成 "art"
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：s = "leetcode"
+ * 输出："cdelotee"
+ *
+ *
+ * 示例 4：
+ *
+ * 输入：s = "ggggggg"
+ * 输出："ggggggg"
+ *
+ *
+ * 示例 5：
+ *
+ * 输入：s = "spo"
+ * 输出："ops"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 500
+ * s 只包含小写英文字母。
+ *
+ *
+ */
+
+// @lc code=start
+// bucket sort
+var sortString = function (s: string): string {
+  const nums = new Array(26).fill(0);
+  for (const char of s) {
+    const index = getIndex(char);
+    nums[index]++;
+  }
+
+  let ret = "";
+  while (ret.length < s.length) {
+    // from small to large at one traverse
+    for (let i = 0; i < 26; i++) {
+      if (nums[i]) {
+        const char = getChar(i);
+        ret += char;
+        nums[i]--;
+      }
+    }
+
+    // from large to small at one traverse
+    for (let i = 25; i >= 0; i--) {
+      if (nums[i]) {
+        const char = getChar(i);
+        ret += char;
+        nums[i]--;
+      }
+    }
+
+    // continue loop
+  }
+  return ret;
+
+  function getIndex(char: string): number {
+    return char.charCodeAt(0) - "a".charCodeAt(0);
+  }
+
+  function getChar(point: number): string {
+    return String.fromCharCode(point + "a".charCodeAt(0));
+  }
+};
+// @lc code=end
