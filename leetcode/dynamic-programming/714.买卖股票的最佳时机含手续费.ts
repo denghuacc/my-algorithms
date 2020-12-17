8f12c0e427af251d2e345c9633008754fe2b316d
Hale Deng <haledeng88@gmail.com>
2020-12-17 09:58:01 +0800

    chore: clock in 714

--- leetcode/dynamic-programming/714.买卖股票的最佳时机含手续费.ts
+++ leetcode/dynamic-programming/714.买卖股票的最佳时机含手续费.ts
@@ -45,18 +45,34 @@
 export {};
 
 // @lc code=start
-// dp
-function maxProfit(prices: number[], fee: number): number {
+// dp1
+var maxProfit = function (prices: number[], fee: number): number {
   const n = prices.length;
-  let dpI0 = 0; // dp[i+0]
-  let dpI1 = -Infinity; // dp[i+1]
+  const dp: number[][] = Array.from(new Array(n), () => new Array(2).fill(0));
 
-  for (let i = 0; i < n; i++) {
-    let tmp = dpI0;
-    dpI0 = Math.max(dpI0, dpI1 + prices[i]);
-    dpI1 = Math.max(dpI1, tmp - prices[i] - fee); // 减去手续费
+  dp[0][0] = 0; //  dp[i][0] 第 i 天交易完后手里没有股票的最大利润
+  dp[0][1] = -prices[0]; // dp[i][1] 第 i 天交易完后手里持有一支股票的最大利润
+
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
   }
 
-  return dpI0;
-}
+  return dp[n - 1][0];
+};
+
+// dp2
+var maxProfit = function (prices: number[], fee: number): number {
+  const n = prices.length;
+  let [sell, buy] = [0, -prices[0]];
+
+  for (let i = 1; i < n; i++) {
+    [sell, buy] = [
+      Math.max(sell, buy + prices[i] - fee),
+      Math.max(buy, sell - prices[i]),
+    ];
+  }
+
+  return sell;
+};
 // @lc code=end
