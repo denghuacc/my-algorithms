e36c59d2eb62d962a465b4a966ede992ff1b8057
Hale Deng <haledeng88@gmail.com>
2020-12-21 23:33:12 +0800

    chore: clock in 746

---
+++ leetcode/dynamic-programming/746.使用最小花费爬楼梯.ts
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=typescript
+ *
+ * [746] 使用最小花费爬楼梯
+ *
+ * https://leetcode-cn.com/problems/min-cost-climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (50.14%)
+ * Likes:    480
+ * Dislikes: 0
+ * Total Accepted:    76.5K
+ * Total Submissions: 141.2K
+ * Testcase Example:  '[0,0,0,0]'
+ *
+ * 数组的每个索引作为一个阶梯，第 i个阶梯对应着一个非负数的体力花费值 cost[i](索引从0开始)。
+ *
+ * 每当你爬上一个阶梯你都要花费对应的体力花费值，然后你可以选择继续爬一个阶梯或者爬两个阶梯。
+ *
+ * 您需要找到达到楼层顶部的最低花费。在开始时，你可以选择从索引为 0 或 1 的元素作为初始阶梯。
+ *
+ * 示例 1:
+ *
+ * 输入: cost = [10, 15, 20]
+ * 输出: 15
+ * 解释: 最低花费是从cost[1]开始，然后走两步即可到阶梯顶，一共花费15。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * 输出: 6
+ * 解释: 最低花费方式是从cost[0]开始，逐个经过那些1，跳过cost[3]，一共花费6。
+ *
+ *
+ * 注意：
+ *
+ *
+ * cost 的长度将会在 [2, 1000]。
+ * 每一个 cost[i] 将会是一个Integer类型，范围为 [0, 999]。
+ *
+ *
+ */
+
+// @lc code=start
+// dp
+var minCostClimbingStairs = function (cost: number[]): number {
+  const n = cost.length;
+  const dp: number[] = new Array(n + 1).fill(0);
+  for (let i = 2; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+  return dp[n];
+};
+
+// dp2
+var minCostClimbingStairs = function (cost: number[]): number {
+  const n = cost.length;
+  let prev = 0;
+  let cur = 0;
+  for (let i = 2; i <= n; i++) {
+    let next = Math.min(cur + cost[i - 1], prev + cost[i - 2]);
+    prev = cur;
+    cur = next;
+  }
+  return cur;
+};
+// @lc code=end
