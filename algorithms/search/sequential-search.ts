813716a0b1b671a1f06cb018cc9f56b06e0a1d29
Hale Deng <haledeng88@gmail.com>
2021-02-17 18:12:31 +0800

    refactor: remove unused code

--- algorithms/search/sequential-search.ts
+++ algorithms/search/sequential-search.ts
@@ -3,7 +3,7 @@
 // 它的机制是，将每一个数据结构中的元素和我们要找的元素做比较。
 // 顺序搜索是最低效的一种搜索算法。
 
-import { defaultEquals, IEqualsFunction, DOES_NOT_EXIST } from "../util";
+import { defaultEquals, DOES_NOT_EXIST } from "../util";
 
 export function sequentialSearch<T>(
   array: T[],
