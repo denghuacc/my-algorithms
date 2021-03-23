636c4cd91993f4d7d01c6c103d626fd045eef27d
Hale Deng <haledeng88@gmail.com>
2021-03-23 10:24:49 +0800

    chore: clock 341

---
+++ leetcode/design/341.扁平化嵌套列表迭代器2.ts
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode.cn id=341 lang=typescript
+ *
+ * [341] 扁平化嵌套列表迭代器
+ *
+ * https://leetcode-cn.com/problems/flatten-nested-list-iterator/description/
+ *
+ * algorithms
+ * Medium (69.23%)
+ * Likes:    229
+ * Dislikes: 0
+ * Total Accepted:    21.3K
+ * Total Submissions: 30.8K
+ * Testcase Example:  '[[1,1],2,[1,1]]'
+ *
+ * 给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+ *
+ * 列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ *
+ * 示例 2:
+ *
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+ *
+ *
+ */
+
+export {};
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+interface NestedInteger {
+  new (value: number): void;
+  isInteger(): boolean;
+  getInteger(): number | null;
+  setInteger(value: number): void;
+  add(elem: NestedInteger): void;
+  getList(): NestedInteger[];
+}
+
+// @lc code=start
+// stack
+class NestedIterator {
+  stack: NestedInteger[] = [];
+
+  constructor(nestedList: NestedInteger[]) {
+    this.stack = nestedList;
+  }
+
+  hasNext(): boolean {
+    while (this.stack.length) {
+      if (this.stack[0].isInteger()) {
+        return true;
+      } else {
+        let list = this.stack[0].getList();
+        this.stack.shift();
+        this.stack.unshift(...list);
+      }
+    }
+    return false;
+  }
+
+  next(): number {
+    return this.stack.shift()!.getInteger()!;
+  }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new NestedIterator(nestedList)
+ * var a: number[] = []
+ * while (obj.hasNext()) a.push(obj.next());
+ */
+// @lc code=end
