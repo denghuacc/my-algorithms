ad7c09b9a930d9b85af2449f04863fe2555110ce
Hale Deng <haledeng88@gmail.com>
2021-03-25 09:50:27 +0800

    chore: clock 82

--- leetcode/linked-list/82.删除排序链表中的重复元素-ii.ts
+++ leetcode/linked-list/82.删除排序链表中的重复元素-ii.ts
@@ -76,4 +76,24 @@ var deleteDuplicates = function (head: ListNode | null): ListNode | null {
     return head;
   }
 };
+
+// iterator
+var deleteDuplicates = function (head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  const dummy = new ListNode(0, head);
+  let cur = dummy;
+
+  while (cur.next && cur.next.next) {
+    if (cur.next.val === cur.next.next.val) {
+      const x = cur.next.val;
+      while (cur.next && cur.next.val === x) {
+        cur.next = cur.next.next;
+      }
+    } else {
+      cur = cur.next;
+    }
+  }
+
+  return dummy.next;
+};
 // @lc code=end
