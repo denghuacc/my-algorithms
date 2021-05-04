3f90afd436f08b8352f03fa785a4c5532d2b19ef
Hale Deng <haledeng88@gmail.com>
2021-05-04 22:28:33 +0800

    chore: clock 1473

---
+++ leetcode/dynamic-programming/1473.粉刷房子-iii.ts
@@ -0,0 +1,137 @@
+/*
+ * @lc app=leetcode.cn id=1473 lang=typescript
+ *
+ * [1473] 粉刷房子 III
+ *
+ * https://leetcode-cn.com/problems/paint-house-iii/description/
+ *
+ * algorithms
+ * Hard (50.90%)
+ * Likes:    121
+ * Dislikes: 0
+ * Total Accepted:    10.4K
+ * Total Submissions: 15.7K
+ * Testcase Example:  '[0,0,0,0,0]\n[[1,10],[10,1],[10,1],[1,10],[5,1]]\n5\n2\n3'
+ *
+ * 在一个小城市里，有 m 个房子排成一排，你需要给每个房子涂上 n 种颜色之一（颜色编号为 1 到 n
+ * ）。有的房子去年夏天已经涂过颜色了，所以这些房子不需要被重新涂色。
+ *
+ * 我们将连续相同颜色尽可能多的房子称为一个街区。（比方说 houses = [1,2,2,3,3,2,1,1] ，它包含 5 个街区  [{1},
+ * {2,2}, {3,3}, {2}, {1,1}] 。）
+ *
+ * 给你一个数组 houses ，一个 m * n 的矩阵 cost 和一个整数 target ，其中：
+ *
+ *
+ * houses[i]：是第 i 个房子的颜色，0 表示这个房子还没有被涂色。
+ * cost[i][j]：是将第 i 个房子涂成颜色 j+1 的花费。
+ *
+ *
+ * 请你返回房子涂色方案的最小总花费，使得每个房子都被涂色后，恰好组成 target 个街区。如果没有可用的涂色方案，请返回 -1 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：houses = [0,0,0,0,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5,
+ * n = 2, target = 3
+ * 输出：9
+ * 解释：房子涂色方案为 [1,2,2,1,1]
+ * 此方案包含 target = 3 个街区，分别是 [{1}, {2,2}, {1,1}]。
+ * 涂色的总花费为 (1 + 1 + 1 + 1 + 5) = 9。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：houses = [0,2,1,2,0], cost = [[1,10],[10,1],[10,1],[1,10],[5,1]], m = 5,
+ * n = 2, target = 3
+ * 输出：11
+ * 解释：有的房子已经被涂色了，在此基础上涂色方案为 [2,2,1,2,2]
+ * 此方案包含 target = 3 个街区，分别是 [{2,2}, {1}, {2,2}]。
+ * 给第一个和最后一个房子涂色的花费为 (10 + 1) = 11。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：houses = [0,0,0,0,0], cost = [[1,10],[10,1],[1,10],[10,1],[1,10]], m = 5,
+ * n = 2, target = 5
+ * 输出：5
+ *
+ *
+ * 示例 4：
+ *
+ * 输入：houses = [3,1,2,3], cost = [[1,1,1],[1,1,1],[1,1,1],[1,1,1]], m = 4, n =
+ * 3, target = 3
+ * 输出：-1
+ * 解释：房子已经被涂色并组成了 4 个街区，分别是 [{3},{1},{2},{3}] ，无法形成 target = 3 个街区。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == houses.length == cost.length
+ * n == cost[i].length
+ * 1 <= m <= 100
+ * 1 <= n <= 20
+ * 1 <= target <= m
+ * 0 <= houses[i] <= n
+ * 1 <= cost[i][j] <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+// dp
+function minCost(
+  houses: number[],
+  cost: number[][],
+  m: number,
+  n: number,
+  target: number
+): number {
+  // 将颜色调整为从 0 开始编号，没有被涂色标记为 -1
+  houses = houses.map((c) => --c);
+  // dp(i,j,k) 表示将 [0, i] 的房子都涂上颜色，最末尾的第 i 个房子的颜色为 j，并且它属于第 k 个街区时，需要的最少花费
+  const dp: number[][][] = new Array(m)
+    .fill(0)
+    .map(() =>
+      new Array(n).fill(0).map(() => new Array(target).fill(Infinity))
+    );
+
+  // dp 所有元素初始化为极大值
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
+      if (houses[i] !== -1 && houses[i] !== j) {
+        continue;
+      }
+
+      for (let k = 0; k < target; ++k) {
+        for (let j0 = 0; j0 < n; ++j0) {
+          if (j === j0) {
+            if (i === 0) {
+              if (k === 0) {
+                dp[i][j][k] = 0;
+              }
+            } else {
+              dp[i][j][k] = Math.min(dp[i][j][k], dp[i - 1][j][k]);
+            }
+          } else if (i > 0 && k > 0) {
+            dp[i][j][k] = Math.min(dp[i][j][k], dp[i - 1][j0][k - 1]);
+          }
+        }
+
+        if (dp[i][j][k] !== Infinity && houses[i] === -1) {
+          dp[i][j][k] += cost[i][j];
+        }
+      }
+    }
+  }
+
+  let ret = Infinity;
+  for (let j = 0; j < n; ++j) {
+    ret = Math.min(ret, dp[m - 1][j][target - 1]);
+  }
+  return ret === Infinity ? -1 : ret;
+}
+// @lc code=end
