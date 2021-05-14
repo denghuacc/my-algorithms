a642e20cfb7d173c16f15e17d53ea57c291773ce
Hale Deng <haledeng88@gmail.com>
2021-05-14 09:57:14 +0800

    chore: clock 12

--- leetcode/math/12.整数转罗马数字.ts
+++ leetcode/math/12.整数转罗马数字.ts
@@ -98,4 +98,19 @@ var intToRoman = function (num: number): string {
   }
   return ret;
 };
+
+// string
+var intToRoman = function (num: number): string {
+  const thousands = ["", "M", "MM", "MMM"];
+  const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+  const roman: string[] = [];
+  roman.push(thousands[Math.floor(num / 1000)]);
+  roman.push(hundreds[Math.floor((num % 1000) / 100)]);
+  roman.push(tens[Math.floor((num % 100) / 10)]);
+  roman.push(ones[num % 10]);
+  return roman.join("");
+};
 // @lc code=end
