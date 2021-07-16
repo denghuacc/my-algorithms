75de6a51c4ff12b59cbf20dfd77fdf5a109249de
Hale Deng <haledeng88@gmail.com>
2021-07-16 10:17:18 +0800

    chore: add solution

---
+++ leetcode/two-pointers/34.在排序数组中查找元素的第一个和最后一个位置.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=golang
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (42.54%)
+ * Likes:    1107
+ * Dislikes: 0
+ * Total Accepted:    285.7K
+ * Total Submissions: 671.7K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 进阶：
+ *
+ *
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0
+ * -10^9
+ * nums 是一个非递减数组
+ * -10^9
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+func searchRange(nums []int, target int) []int {
+	n := len(nums)
+	l := 0
+	r := n - 1
+	firstIndex := -1
+	lastIndex := -1
+	for l <= r {
+		if nums[l] == target {
+			firstIndex = l
+			break
+		}
+		l++
+	}
+
+	for l <= r {
+		if nums[r] == target {
+			lastIndex = r
+			break
+		}
+		r--
+	}
+
+	return []int{firstIndex, lastIndex}
+}
+
+// @lc code=end
