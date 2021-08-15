69f625fffd4ca04e0b26f6778cc27b487a1aa2be
Hale Deng <haledeng88@gmail.com>
2021-08-15 23:33:20 +0800

    chore: clock 576

---
+++ leetcode/dynamic-programming/576.出界的路径数.go
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=576 lang=golang
+ *
+ * [576] 出界的路径数
+ *
+ * https://leetcode-cn.com/problems/out-of-boundary-paths/description/
+ *
+ * algorithms
+ * Medium (39.63%)
+ * Likes:    191
+ * Dislikes: 0
+ * Total Accepted:    21.4K
+ * Total Submissions: 46.4K
+ * Testcase Example:  '2\n2\n2\n0\n0'
+ *
+ * 给你一个大小为 m x n 的网格和一个球。球的起始坐标为 [startRow, startColumn]
+ * 。你可以将球移到在四个方向上相邻的单元格内（可以穿过网格边界到达网格之外）。你 最多 可以移动 maxMove 次球。
+ *
+ * 给你五个整数 m、n、maxMove、startRow 以及 startColumn ，找出并返回可以将球移出边界的路径数量。因为答案可能非常大，返回对
+ * 10^9 + 7 取余 后的结果。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+ * 输出：6
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1
+ * 输出：12
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= m, n <= 50
+ * 0 <= maxMove <= 50
+ * 0 <= startRow < m
+ * 0 <= startColumn < n
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+const mod int = 1e9 + 7
+
+var dirs = []struct{ x, y int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} // 上下左右
+
+// dp
+func findPaths(m, n, maxMove, startRow, startColumn int) (ret int) {
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	dp[startRow][startColumn] = 1
+	for i := 0; i < maxMove; i++ {
+		dpNew := make([][]int, m)
+		for j := range dpNew {
+			dpNew[j] = make([]int, n)
+		}
+		for j := 0; j < m; j++ {
+			for k := 0; k < n; k++ {
+				count := dp[j][k]
+				if count > 0 {
+					for _, dir := range dirs {
+						j1, k1 := j+dir.x, k+dir.y
+						if j1 >= 0 && j1 < m && k1 >= 0 && k1 < n {
+							dpNew[j1][k1] = (dpNew[j1][k1] + count) % mod
+						} else {
+							ret = (ret + count) % mod
+						}
+					}
+				}
+			}
+		}
+		dp = dpNew
+	}
+	return
+}
+
+// @lc code=end
