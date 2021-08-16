9d6635cb33a3e093de2b354189caec97c4eb9acf
Hale Deng <haledeng88@gmail.com>
2021-08-16 10:24:26 +0800

    chore: clock 526

---
+++ leetcode/backtracking/526.优美的排列.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=526 lang=golang
+ *
+ * [526] 优美的排列
+ *
+ * https://leetcode-cn.com/problems/beautiful-arrangement/description/
+ *
+ * algorithms
+ * Medium (68.32%)
+ * Likes:    166
+ * Dislikes: 0
+ * Total Accepted:    15.5K
+ * Total Submissions: 22.7K
+ * Testcase Example:  '2'
+ *
+ * 假设有从 1 到 N 的 N 个整数，如果从这 N 个数字中成功构造出一个数组，使得数组的第 i 位 (1 <= i <= N)
+ * 满足如下两个条件中的一个，我们就称这个数组为一个优美的排列。条件：
+ *
+ *
+ * 第 i 位的数字能被 i 整除
+ * i 能被第 i 位上的数字整除
+ *
+ *
+ * 现在给定一个整数 N，请问可以构造多少个优美的排列？
+ *
+ * 示例1:
+ *
+ *
+ * 输入: 2
+ * 输出: 2
+ * 解释:
+ *
+ * 第 1 个优美的排列是 [1, 2]:
+ * ⁠ 第 1 个位置（i=1）上的数字是1，1能被 i（i=1）整除
+ * ⁠ 第 2 个位置（i=2）上的数字是2，2能被 i（i=2）整除
+ *
+ * 第 2 个优美的排列是 [2, 1]:
+ * ⁠ 第 1 个位置（i=1）上的数字是2，2能被 i（i=1）整除
+ * ⁠ 第 2 个位置（i=2）上的数字是1，i（i=2）能被 1 整除
+ *
+ *
+ * 说明:
+ *
+ *
+ * N 是一个正整数，并且不会超过15。
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+// backtracking
+func countArrangement(n int) (ret int) {
+	vis := make([]bool, n+1)
+	match := make([][]int, n+1)
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			if i%j == 0 || j%i == 0 {
+				match[i] = append(match[i], j)
+			}
+		}
+	}
+
+	var backtrack func(int)
+	backtrack = func(index int) {
+		if index > n {
+			ret++
+			return
+		}
+		for _, x := range match[index] {
+			if !vis[x] {
+				vis[x] = true
+				backtrack(index + 1)
+				vis[x] = false
+			}
+		}
+	}
+
+	backtrack(1)
+	return
+}
+
+// @lc code=end
