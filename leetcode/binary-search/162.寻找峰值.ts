a6abb1be593682be568734f0021294fb47ea8c8d
Hale Deng <haledeng88@gmail.com>
2021-09-15 10:42:03 +0800

    chore: clock 162

---
+++ leetcode/binary-search/162.寻找峰值.ts
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=typescript
+ *
+ * [162] 寻找峰值
+ *
+ * https://leetcode-cn.com/problems/find-peak-element/description/
+ *
+ * algorithms
+ * Medium (49.48%)
+ * Likes:    554
+ * Dislikes: 0
+ * Total Accepted:    133.8K
+ * Total Submissions: 269.1K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ *
+ * 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+ *
+ * 你可以假设 nums[-1] = nums[n] = -∞ 。
+ *
+ * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3,1]
+ * 输出：2
+ * 解释：3 是峰值元素，你的函数应该返回其索引 2。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,2,1,3,5,6,4]
+ * 输出：1 或 5
+ * 解释：你的函数可以返回索引 1，其峰值元素为 2；
+ * 或者返回索引 5， 其峰值元素为 6。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 1000
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 对于所有有效的 i 都有 nums[i] != nums[i + 1]
+ *
+ *
+ */
+
+// @lc code=start
+var findPeakElement = function (nums: number[]): number {
+  let idx = 1;
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      idx = i;
+    }
+  }
+  return idx;
+};
+
+// binary search
+var findPeakElement = function (nums: number[]): number {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = l + ((r - l) >> 1);
+    if (nums[m] < nums[m + 1]) {
+      l = m + 1;
+    } else {
+      r = m;
+    }
+  }
+  return l;
+};
+// @lc code=end
