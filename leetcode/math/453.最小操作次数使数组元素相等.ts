a5ee80f188666475c66ec37d2d9b4f6fb30f3850
Hale Deng <haledeng88@gmail.com>
2021-10-20 10:18:12 +0800

    chore: clock 453

---
+++ leetcode/math/453.最小操作次数使数组元素相等.ts
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=453 lang=typescript
+ *
+ * [453] 最小操作次数使数组元素相等
+ *
+ * https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/description/
+ *
+ * algorithms
+ * Easy (57.84%)
+ * Likes:    300
+ * Dislikes: 0
+ * Total Accepted:    34.2K
+ * Total Submissions: 59K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给你一个长度为 n 的整数数组，每次操作将会使 n - 1 个元素增加 1 。返回让数组所有元素相等的最小操作次数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：3
+ * 解释：
+ * 只需要3次操作（注意每次操作会增加两个元素的值）：
+ * [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,1,1]
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * 答案保证符合 32-bit 整数
+ *
+ *
+ */
+
+// @lc code=start
+// math
+function minMoves(nums: number[]): number {
+  const min = Math.min(...nums);
+  let ret = 0;
+  for (const num of nums) {
+    ret += num - min;
+  }
+  return ret;
+}
+// @lc code=end
