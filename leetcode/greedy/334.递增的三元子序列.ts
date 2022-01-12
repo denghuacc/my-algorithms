234f53f88890bcd25d3c4bdb254321b0b0e956c9
Hale Deng <haledeng88@gmail.com>
2022-01-12 11:00:59 +0800

    chore: clock 334

---
+++ leetcode/greedy/334.递增的三元子序列.ts
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=334 lang=typescript
+ *
+ * [334] 递增的三元子序列
+ *
+ * https://leetcode-cn.com/problems/increasing-triplet-subsequence/description/
+ *
+ * algorithms
+ * Medium (41.23%)
+ * Likes:    430
+ * Dislikes: 0
+ * Total Accepted:    59K
+ * Total Submissions: 140.9K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+ *
+ * 如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，使得 nums[i] < nums[j] < nums[k] ，返回
+ * true ；否则，返回 false 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,2,3,4,5]
+ * 输出：true
+ * 解释：任何 i < j < k 的三元组都满足题意
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [5,4,3,2,1]
+ * 输出：false
+ * 解释：不存在满足题意的三元组
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [2,1,5,0,4,6]
+ * 输出：true
+ * 解释：三元组 (3, 4, 5) 满足题意，因为 nums[3] == 0 < nums[4] == 4 < nums[5] == 6
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 5 * 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能实现时间复杂度为 O(n) ，空间复杂度为 O(1) 的解决方案吗？
+ *
+ */
+
+// @lc code=start
+// greedy
+function increasingTriplet(nums: number[]): boolean {
+  const n = nums.length;
+  if (n < 3) {
+    return false;
+  }
+  let first = nums[0];
+  let second = Infinity;
+  let i = 1;
+  while (i < n) {
+    const num = nums[i];
+    if (num > second) {
+      return true;
+    } else if (num > first) {
+      second = num;
+    } else {
+      first = num;
+    }
+    i++;
+  }
+  return false;
+}
+// @lc code=end
