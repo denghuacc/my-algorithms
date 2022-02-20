f2e8eaad2aebeaca94dc8ce1484ae7c2d8345863
Hale Deng <haledeng88@gmail.com>
2022-02-20 09:53:06 +0800

    chore: clock 717

---
+++ leetcode/array/717.1-比特与-2-比特字符.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=717 lang=typescript
+ *
+ * [717] 1比特与2比特字符
+ *
+ * https://leetcode-cn.com/problems/1-bit-and-2-bit-characters/description/
+ *
+ * algorithms
+ * Easy (52.47%)
+ * Likes:    217
+ * Dislikes: 0
+ * Total Accepted:    37.9K
+ * Total Submissions: 72.3K
+ * Testcase Example:  '[1,0,0]'
+ *
+ * 有两种特殊字符：
+ *
+ *
+ * 第一种字符可以用一个比特 0 来表示
+ * 第二种字符可以用两个比特(10 或 11)来表示、
+ *
+ *
+ * 给定一个以 0 结尾的二进制数组 bits ，如果最后一个字符必须是一位字符，则返回 true 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: bits = [1, 0, 0]
+ * 输出: true
+ * 解释: 唯一的编码方式是一个两比特字符和一个一比特字符。
+ * 所以最后一个字符是一比特字符。
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: bits = [1, 1, 1, 0]
+ * 输出: false
+ * 解释: 唯一的编码方式是两比特字符和两比特字符。
+ * 所以最后一个字符不是一比特字符。
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= bits.length <= 1000
+ * bits[i] == 0 or 1
+ *
+ *
+ */
+
+// @lc code=start
+var isOneBitCharacter = function (bits: number[]): boolean {
+  const n = bits.length;
+  let i = 0;
+  while (i < n - 1) {
+    if (bits[i] === 0) {
+      i += 1;
+    } else {
+      i += 2;
+    }
+  }
+  return i === n - 1;
+};
+
+var isOneBitCharacter = function (bits: number[]): boolean {
+  const n = bits.length;
+  let i = n - 2;
+  while (i >= 0 && bits[i] === 1) {
+    i -= 1;
+  }
+  return (n - 1) % 2 === 0;
+};
+// @lc code=end
