fd92201250852fe16d9db80bf4ba0274d887fa1c
Hale Deng <haledeng88@gmail.com>
2022-04-03 11:27:41 +0800

    chore: clock 744

---
+++ leetcode/binary-search/744.寻找比目标字母大的最小字母.ts
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=744 lang=typescript
+ *
+ * [744] 寻找比目标字母大的最小字母
+ *
+ * https://leetcode-cn.com/problems/find-smallest-letter-greater-than-target/description/
+ *
+ * algorithms
+ * Easy (45.73%)
+ * Likes:    171
+ * Dislikes: 0
+ * Total Accepted:    58.5K
+ * Total Submissions: 123.2K
+ * Testcase Example:  '["c","f","j"]\n"a"'
+ *
+ * 给你一个排序后的字符列表 letters ，列表中只包含小写英文字母。另给出一个目标字母
+ * target，请你寻找在这一有序列表里比目标字母大的最小字母。
+ *
+ * 在比较时，字母是依序循环出现的。举个例子：
+ *
+ *
+ * 如果目标字母 target = 'z' 并且字符列表为 letters = ['a', 'b']，则答案返回 'a'
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入: letters = ["c", "f", "j"]，target = "a"
+ * 输出: "c"
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: letters = ["c","f","j"], target = "c"
+ * 输出: "f"
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: letters = ["c","f","j"], target = "d"
+ * 输出: "f"
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 2 <= letters.length <= 10^4
+ * letters[i] 是一个小写字母
+ * letters 按非递减顺序排序
+ * letters 最少包含两个不同的字母
+ * target 是一个小写字母
+ *
+ *
+ */
+
+// @lc code=start
+var nextGreatestLetter = function (letters: string[], target: string): string {
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] > target) {
+      return letters[i];
+    }
+  }
+  return letters[0];
+};
+
+// binary search
+var nextGreatestLetter = function (letters: string[], target: string): string {
+  let left = 0;
+  let right = letters.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (letters[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if (letters[left] > target) {
+    return letters[left];
+  }
+  return letters[0];
+};
+// @lc code=end
