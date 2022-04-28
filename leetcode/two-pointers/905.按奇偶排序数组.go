a5f14de4e405771862783c6ab3ff2b5f20157549
Hale Deng <haledeng88@gmail.com>
2022-04-28 09:56:18 +0800

    chore: clock 905

---
+++ leetcode/two-pointers/905.按奇偶排序数组.go
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=905 lang=golang
+ *
+ * [905] 按奇偶排序数组
+ *
+ * https://leetcode-cn.com/problems/sort-array-by-parity/description/
+ *
+ * algorithms
+ * Easy (69.98%)
+ * Likes:    262
+ * Dislikes: 0
+ * Total Accepted:    76K
+ * Total Submissions: 107.6K
+ * Testcase Example:  '[3,1,2,4]'
+ *
+ * 给你一个整数数组 nums，将 nums 中的的所有偶数元素移动到数组的前面，后跟所有奇数元素。
+ *
+ * 返回满足此条件的 任一数组 作为答案。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,1,2,4]
+ * 输出：[2,4,3,1]
+ * 解释：[4,2,3,1]、[2,4,1,3] 和 [4,2,1,3] 也会被视作正确答案。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [0]
+ * 输出：[0]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 5000
+ * 0 <= nums[i] <= 5000
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+func sortArrayByParity(nums []int) []int {
+	i, j := 0, len(nums)-1
+
+	for i < j {
+		if nums[i]%2 == 0 {
+			i++
+		} else if nums[j]%2 == 1 {
+			j--
+		} else {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j--
+		}
+	}
+
+	return nums
+}
+
+// @lc code=end
