605d0a3ff433a6db288a06c60bf3929724e7e650
Hale Deng <haledeng88@gmail.com>
2022-05-08 09:19:25 +0800

    chore: clock 442

---
+++ leetcode/array/442.数组中重复的数据.go
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=golang
+ *
+ * [442] 数组中重复的数据
+ *
+ * https://leetcode-cn.com/problems/find-all-duplicates-in-an-array/description/
+ *
+ * algorithms
+ * Medium (70.48%)
+ * Likes:    502
+ * Dislikes: 0
+ * Total Accepted:    58.1K
+ * Total Submissions: 81.6K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * 给你一个长度为 n 的整数数组 nums ，其中 nums 的所有整数都在范围 [1, n] 内，且每个整数出现 一次 或 两次 。请你找出所有出现
+ * 两次 的整数，并以数组形式返回。
+ *
+ * 你必须设计并实现一个时间复杂度为 O(n) 且仅使用常量额外空间的算法解决此问题。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [4,3,2,7,8,2,3,1]
+ * 输出：[2,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,1,2]
+ * 输出：[1]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：nums = [1]
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 10^5
+ * 1 <= nums[i] <= n
+ * nums 中的每个元素出现 一次 或 两次
+ *
+ *
+ */
+
+package leetcode
+
+// @lc code=start
+func findDuplicates(nums []int) []int {
+	var res []int
+	for _, num := range nums {
+		x := abs(num)
+		if nums[x-1] < 0 {
+			res = append(res, x)
+		} else {
+			nums[x-1] = -nums[x-1]
+		}
+	}
+	return res
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// @lc code=end
