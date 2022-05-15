9065dee5db1e96bc4e587337f77747b28003a893
Alan Deng <alanhaledc@gmail.com>
2022-05-15 16:42:34 +0800

    chore: clock 812

--- leetcode/array/812.最大三角形面积.ts
+++ leetcode/array/812.最大三角形面积.ts
@@ -41,15 +41,17 @@
 var largestTriangleArea = function (points: number[][]): number {
   let n = points.length;
   let ret = 0;
-  for (let i = 0; i < n; ++i) {
-    for (let j = i + 1; j < n; ++j) {
-      for (let k = j + 1; k < n; ++k) {
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
         ret = Math.max(ret, area(points[i], points[j], points[k]));
       }
     }
   }
   return ret;
 
+  // 设三角形三个顶点为 a(x1, y1), b(x2, y2), c(x3, y3)，则三角形面积为：
+  // S = 0.5 * | x1y2+x2y3+x3y1-y1x2-y2x3-y3x1 |
   function area(a: number[], b: number[], c: number[]) {
     return (
       0.5 *
