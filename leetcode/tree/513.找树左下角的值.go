ebd02380797a1064f8010c0d866114ba791d429e
Alan Deng <alanhaledc@gmail.com>
2022-06-22 12:04:26 +0800

    chore: clock 513

---
+++ leetcode/tree/513.找树左下角的值.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=golang
+ *
+ * [513] 找树左下角的值
+ *
+ * https://leetcode.cn/problems/find-bottom-left-tree-value/description/
+ *
+ * algorithms
+ * Medium (73.13%)
+ * Likes:    306
+ * Dislikes: 0
+ * Total Accepted:    104.6K
+ * Total Submissions: 141.7K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+ *
+ * 假设二叉树中至少有一个节点。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入: root = [2,1,3]
+ * 输出: 1
+ *
+ *
+ * 示例 2:
+ *
+ * ⁠
+ *
+ *
+ * 输入: [1,2,3,4,null,5,6,null,null,7]
+ * 输出: 7
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 二叉树的节点个数的范围是 [1,10^4]
+ * -2^31
+ *
+ *
+ */
+
+package leetcode
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+func findBottomLeftValue(root *TreeNode) (res int) {
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		res = node.Val
+	}
+	return
+}
+
+// @lc code=end
