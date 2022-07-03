908424eb27553520b74c998e1bc574257fcd541e
Alan Deng <alanhaledc@gmail.com>
2022-07-03 18:21:42 +0800

    chore: clock 556

---
+++ leetcode/array/556.下一个更大元素-iii.go
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=556 lang=golang
+ *
+ * [556] 下一个更大元素 III
+ *
+ * https://leetcode.cn/problems/next-greater-element-iii/description/
+ *
+ * algorithms
+ * Medium (33.58%)
+ * Likes:    251
+ * Dislikes: 0
+ * Total Accepted:    26.3K
+ * Total Submissions: 75K
+ * Testcase Example:  '12'
+ *
+ * 给你一个正整数 n ，请你找出符合条件的最小整数，其由重新排列 n 中存在的每位数字组成，并且其值大于 n 。如果不存在这样的正整数，则返回 -1 。
+ *
+ * 注意 ，返回的整数应当是一个 32 位整数 ，如果存在满足题意的答案，但不是 32 位整数 ，同样返回 -1 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 12
+ * 输出：21
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 21
+ * 输出：-1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ *
+ *
+ */
+
+package leetcode
+
+import (
+	"math"
+	"strconv"
+)
+
+// @lc code=start
+func nextGreaterElement(n int) int {
+	nums := []byte(strconv.Itoa(n))
+	i := len(nums) - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i < 0 {
+		return -1
+	}
+	j := len(nums) - 1
+	for j >= 0 && nums[i] >= nums[j] {
+		j--
+	}
+	nums[i], nums[j] = nums[j], nums[i]
+	reverse(nums[i+1:])
+	res, _ := strconv.Atoi(string(nums))
+	if res > math.MaxInt32 {
+		return -1
+	}
+	return res
+
+}
+
+func reverse(a []byte) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+
+// @lc code=end
