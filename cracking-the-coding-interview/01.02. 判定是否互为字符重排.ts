0517b7d13076b1afb128399befba877f30d148a0
Alan Deng <alanhaledc@gmail.com>
2022-09-27 10:51:39 +0800

    chore: clock 01.02

---
+++ cracking-the-coding-interview/01.02. 判定是否互为字符重排.ts
@@ -0,0 +1,43 @@
+/**
+ * 
+给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+
+示例 1：
+输入: s1 = "abc", s2 = "bca"
+输出: true 
+
+示例 2：
+输入: s1 = "abc", s2 = "bad"
+输出: false
+
+说明：
+0 <= len(s1) <= 100
+0 <= len(s2) <= 100
+ * 
+ */
+
+// sorting
+var CheckPermutation = function (s1: string, s2: string): boolean {
+  const sortedS1 = s1.split("").sort().join("");
+  const sortedS2 = s2.split("").sort().join("");
+  return sortedS1 === sortedS2;
+};
+
+// hash table
+var CheckPermutation = function (s1: string, s2: string): boolean {
+  const map: Map<string, number> = new Map();
+  for (const ch of s1) {
+    map.set(ch, (map.get(ch) ?? 0) + 1);
+  }
+  for (const ch of s2) {
+    if (map.has(ch)) {
+      map.set(ch, (map.get(ch) ?? 0) - 1);
+      if (map.get(ch)! < 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
