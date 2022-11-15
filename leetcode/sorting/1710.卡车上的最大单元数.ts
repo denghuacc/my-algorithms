a924b91e544830cc2e20f187db6eda5753d8514f
Alan Deng <alanhaledc@gmail.com>
2022-11-15 10:57:14 +0800

    chore: clock 1710

---
+++ leetcode/sorting/1710.卡车上的最大单元数.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=1710 lang=typescript
+ *
+ * [1710] 卡车上的最大单元数
+ *
+ * https://leetcode.cn/problems/maximum-units-on-a-truck/description/
+ *
+ * algorithms
+ * Easy (69.65%)
+ * Likes:    67
+ * Dislikes: 0
+ * Total Accepted:    27K
+ * Total Submissions: 37.2K
+ * Testcase Example:  '[[1,3],[2,2],[3,1]]\n4'
+ *
+ * 请你将一些箱子装在 一辆卡车 上。给你一个二维数组 boxTypes ，其中 boxTypes[i] = [numberOfBoxesi,
+ * numberOfUnitsPerBoxi] ：
+ *
+ *
+ * numberOfBoxesi 是类型 i 的箱子的数量。
+ * numberOfUnitsPerBoxi 是类型 i 每个箱子可以装载的单元数量。
+ *
+ *
+ * 整数 truckSize 表示卡车上可以装载 箱子 的 最大数量 。只要箱子数量不超过 truckSize ，你就可以选择任意箱子装到卡车上。
+ *
+ * 返回卡车可以装载 单元 的 最大 总数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+ * 输出：8
+ * 解释：箱子的情况如下：
+ * - 1 个第一类的箱子，里面含 3 个单元。
+ * - 2 个第二类的箱子，每个里面含 2 个单元。
+ * - 3 个第三类的箱子，每个里面含 1 个单元。
+ * 可以选择第一类和第二类的所有箱子，以及第三类的一个箱子。
+ * 单元总数 = (1 * 3) + (2 * 2) + (1 * 1) = 8
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+ * 输出：91
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 1 i, numberOfUnitsPerBoxi
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+// sorting
+function maximumUnits(boxTypes: number[][], truckSize: number): number {
+  let res = 0;
+  boxTypes.sort((a, b) => b[1] - a[1]);
+  for (const boxType of boxTypes) {
+    for (let i = 0; i < boxType[0]; i++) {
+      if (truckSize > 0) {
+        res += boxType[1];
+        truckSize--;
+      } else {
+        return res;
+      }
+    }
+  }
+  return res;
+}
+// @lc code=end
