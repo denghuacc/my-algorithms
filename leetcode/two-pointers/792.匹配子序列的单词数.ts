91cbe0624db5b0699866aa4a1d4687dcec6f4fa1
Alan Deng <alanhaledc@gmail.com>
2022-11-17 18:12:19 +0800

    chore: clock 792

--- leetcode/two-pointers/792.匹配子序列的单词数.ts
+++ leetcode/two-pointers/792.匹配子序列的单词数.ts
@@ -13,12 +13,12 @@
  * Total Submissions: 10.9K
  * Testcase Example:  '"abcde"\n["a","bb","acd","ace"]'
  *
- * 给定字符串 S 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
+ * 给定字符串 s 和单词字典 words, 求 words[i] 中是 S 的子序列的单词个数。
  *
  *
  * 示例:
  * 输入:
- * S = "abcde"
+ * s = "abcde"
  * words = ["a", "bb", "acd", "ace"]
  * 输出: 3
  * 解释: 有三个是 S 的子序列的单词: "a", "acd", "ace"。
@@ -28,7 +28,7 @@
  *
  *
  * 所有在words和 S 里的单词都只由小写字母组成。
- * S 的长度在 [1, 50000]。
+ * s 的长度在 [1, 50000]。
  * words 的长度在 [1, 5000]。
  * words[i]的长度在[1, 50]。
  *
@@ -37,52 +37,43 @@
 
 // @lc code=start
 // two pointers
-var numMatchingSubseq = function (S: string, words: string[]): number {
-  let ret = 0;
+var numMatchingSubseq = function (s: string, words: string[]): number {
+  let res = 0;
   for (let i = 0; i < words.length; i++) {
     let j = 0;
     let k = 0;
-
-    while (j < words[i].length && k < S.length) {
-      if (words[i][j] === S[k]) j++;
+    while (j < words[i].length && k < s.length) {
+      if (words[i][j] === s[k]) {
+        j++;
+      }
       k++;
     }
-    if (j === words[i].length) ret++;
+    if (j === words[i].length) {
+      res++;
+    }
   }
-
-  return ret;
+  return res;
 };
 
-// bucket sort
-var numMatchingSubseq = function (S: string, words: string[]): number {
-  const bucket: { word: string; index: number }[][] = Array.from(
-    new Array(26),
-    () => []
-  );
-  for (const word of words) {
-    bucket[word.charCodeAt(0) - 97].push({
-      word,
-      index: 0,
-    });
+// two pointers
+var numMatchingSubseq = function (s: string, words: string[]): number {
+  const bucket: [number, number][][] = Array.from(new Array(26), () => []);
+  for (let i = 0; i < words.length; i++) {
+    bucket[words[i].charCodeAt(0) - 97].push([i, 0]);
   }
-  let count = 0;
-  for (const c of S) {
-    const list = bucket[c.charCodeAt(0) - 97];
-    bucket[c.charCodeAt(0) - 97] = [];
-    const len = list.length;
+  let res = 0;
+  for (const c of s) {
+    const len = bucket[c.charCodeAt(0) - 97].length;
     for (let i = 0; i < len; i++) {
-      let { word, index } = list[i];
-      if (index === word.length - 1) {
-        count++;
+      let [i, j] = bucket[c.charCodeAt(0) - 97].shift()!;
+      if (j === words[i].length - 1) {
+        res++;
       } else {
-        index++;
-        bucket[word.charCodeAt(index) - 97].push({
-          word,
-          index,
-        });
+        j++;
+        bucket[words[i].charCodeAt(j) - 97].push([i, j]);
       }
     }
   }
-  return count;
+  return res;
 };
 // @lc code=end
