e68e80db6981136eee8480ae6a2d2bf157d79517
Alan Deng <alanhaledc@gmail.com>
2022-11-28 11:05:12 +0800

    chore: clock 813

---
+++ leetcode/dynamic-programming/813.最大平均值和的分组.rs
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=813 lang=rust
+ *
+ * [813] 最大平均值和的分组
+ *
+ * https://leetcode.cn/problems/largest-sum-of-averages/description/
+ *
+ * algorithms
+ * Medium (56.40%)
+ * Likes:    264
+ * Dislikes: 0
+ * Total Accepted:    13.1K
+ * Total Submissions: 22.7K
+ * Testcase Example:  '[9,1,2,3,9]\n3'
+ *
+ * 给定数组 nums 和一个整数 k 。我们将给定的数组 nums 分成 最多 k 个相邻的非空子数组 。 分数 由每个子数组内的平均值的总和构成。
+ *
+ * 注意我们必须使用 nums 数组中的每一个数进行分组，并且分数不一定需要是整数。
+ *
+ * 返回我们所能得到的最大 分数 是多少。答案误差在 10^-6 内被视为是正确的。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [9,1,2,3,9], k = 3
+ * 输出: 20.00000
+ * 解释:
+ * nums 的最优分组是[9], [1, 2, 3], [9]. 得到的分数是 9 + (1 + 2 + 3) / 3 + 9 = 20.
+ * 我们也可以把 nums 分成[9, 1], [2], [3, 9].
+ * 这样的分组得到的分数为 5 + 2 + 6 = 13, 但不是最大值.
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [1,2,3,4,5,6,7], k = 4
+ * 输出: 20.50000
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 10^4
+ * 1 <= k <= nums.length
+ *
+ *
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn largest_sum_of_averages(nums: Vec<i32>, k: i32) -> f64 {
+        let n = nums.len();
+        let mut prefix_sum = vec![0; n + 1];
+        for i in 0..n {
+            prefix_sum[i + 1] = prefix_sum[i] + nums[i];
+        }
+        let mut dp = vec![vec![0.0; k as usize + 1]; n + 1];
+        for i in 1..=n {
+            dp[i][1] = (prefix_sum[i] as f64) / i as f64;
+        }
+        for j in 2..=k as usize {
+            for i in j..=n {
+                for x in j - 1..i {
+                    dp[i][j] = f64::max(
+                        dp[i][j],
+                        dp[x][j - 1] + (prefix_sum[i] - prefix_sum[x]) as f64 / (i - x) as f64,
+                    )
+                }
+            }
+        }
+        return dp[n][k as usize];
+    }
+}
+// @lc code=end
