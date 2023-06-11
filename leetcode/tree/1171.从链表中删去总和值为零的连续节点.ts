0c13c823d1060d903f5d88b8b8d55d41d17cd8f1
Alan Deng <alanhaledc@gmail.com>
2023-06-11 18:11:55 +0800

    chore: clock 1171

---
+++ leetcode/tree/1171.从链表中删去总和值为零的连续节点.ts
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=1171 lang=typescript
+ *
+ * [1171] 从链表中删去总和值为零的连续节点
+ *
+ * https://leetcode.cn/problems/remove-zero-sum-consecutive-nodes-from-linked-list/description/
+ *
+ * algorithms
+ * Medium (48.45%)
+ * Likes:    257
+ * Dislikes: 0
+ * Total Accepted:    27.7K
+ * Total Submissions: 54K
+ * Testcase Example:  '[1,2,-3,3,1]'
+ *
+ * 给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+ *
+ * 删除完毕后，请你返回最终结果链表的头节点。
+ *
+ *
+ *
+ * 你可以返回任何满足题目要求的答案。
+ *
+ * （注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,2,-3,3,1]
+ * 输出：[3,1]
+ * 提示：答案 [1,2,1] 也是正确的。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：head = [1,2,3,-3,4]
+ * 输出：[1,2,4]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1,2,3,-3,-2]
+ * 输出：[1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 给你的链表中可能有 1 到 1000 个节点。
+ * 对于链表中的每个节点，节点的值：-1000 <= node.val <= 1000.
+ *
+ *
+ */
+
+export {};
+
+//  Definition for singly-linked list.
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// @lc code=start
+function removeZeroSumSublists(head: ListNode | null): ListNode | null {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  const seen: Record<number, ListNode> = {};
+  let prefix = 0;
+  for (let node = dummy; node !== null; node = node.next!) {
+    prefix += node.val;
+    seen[prefix] = node;
+  }
+  prefix = 0;
+  for (let node = dummy; node !== null; node = node.next!) {
+    prefix += node.val;
+    node.next = seen[prefix].next;
+  }
+  return dummy.next;
+}
+// @lc code=end
