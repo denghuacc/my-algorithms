0129caebe274d84e2f05de11ddc727dc24f6f95b
Alan Deng <alanhaledc@gmail.com>
2023-11-29 12:12:10 +0800

    chore: clock 2336

---
+++ leetcode/design/2336.无限集中的最小数字2.ts
@@ -0,0 +1,186 @@
+/*
+ * @lc app=leetcode.cn id=2336 lang=typescript
+ *
+ * [2336] 无限集中的最小数字
+ *
+ * https://leetcode.cn/problems/smallest-number-in-infinite-set/description/
+ *
+ * algorithms
+ * Medium (69.39%)
+ * Likes:    53
+ * Dislikes: 0
+ * Total Accepted:    19.4K
+ * Total Submissions: 27K
+ * Testcase Example:  '["SmallestInfiniteSet","addBack","popSmallest","popSmallest","popSmallest","addBack","popSmallest","popSmallest","popSmallest"]\n' +
+  '[[],[2],[],[],[],[1],[],[],[]]'
+ *
+ * 现有一个包含所有正整数的集合 [1, 2, 3, 4, 5, ...] 。
+ * 
+ * 实现 SmallestInfiniteSet 类：
+ * 
+ * 
+ * SmallestInfiniteSet() 初始化 SmallestInfiniteSet 对象以包含 所有 正整数。
+ * int popSmallest() 移除 并返回该无限集中的最小整数。
+ * void addBack(int num) 如果正整数 num 不 存在于无限集中，则将一个 num 添加 到该无限集中。
+ * 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入
+ * ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest",
+ * "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+ * [[], [2], [], [], [], [1], [], [], []]
+ * 输出
+ * [null, null, 1, 2, 3, null, 1, 4, 5]
+ * 
+ * 解释
+ * SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+ * smallestInfiniteSet.addBack(2);    // 2 已经在集合中，所以不做任何变更。
+ * smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 是最小的整数，并将其从集合中移除。
+ * smallestInfiniteSet.popSmallest(); // 返回 2 ，并将其从集合中移除。
+ * smallestInfiniteSet.popSmallest(); // 返回 3 ，并将其从集合中移除。
+ * smallestInfiniteSet.addBack(1);    // 将 1 添加到该集合中。
+ * smallestInfiniteSet.popSmallest(); // 返回 1 ，因为 1 在上一步中被添加到集合中，
+ * ⁠                                  // 且 1 是最小的整数，并将其从集合中移除。
+ * smallestInfiniteSet.popSmallest(); // 返回 4 ，并将其从集合中移除。
+ * smallestInfiniteSet.popSmallest(); // 返回 5 ，并将其从集合中移除。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= num <= 1000
+ * 最多调用 popSmallest 和 addBack 方法 共计 1000 次
+ * 
+ * 
+ */
+
+export {};
+
+// @lc code=start
+class SmallestInfiniteSet {
+  set: Set<number>;
+  smallest: number;
+  pq: Heap<number>;
+
+  constructor() {
+    this.set = new Set();
+    this.smallest = 1;
+    this.pq = new Heap((a, b) => a < b);
+  }
+
+  popSmallest(): number {
+    if (this.set.size === 0) {
+      const smallest = this.smallest;
+      this.smallest++;
+      return smallest;
+    }
+
+    const smallest = this.pq.pop()!;
+    this.set.delete(smallest);
+    return smallest;
+  }
+
+  addBack(num: number): void {
+    if (num < this.smallest && !this.set.has(num)) {
+      this.set.add(num);
+      this.pq.push(num);
+    }
+  }
+}
+
+class Heap<T> {
+  items: T[] = [];
+  compare: (a: T, b: T) => boolean;
+
+  constructor(compare: (a: T, b: T) => boolean = (a, b) => a > b) {
+    this.items = [];
+    this.compare = compare;
+  }
+
+  get size(): number {
+    return this.items.length;
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  private parent(index: number): number {
+    if (index !== 0) {
+      return Math.floor((index - 1) / 2);
+    } else {
+      return 0;
+    }
+  }
+
+  private leftChild(index: number): number {
+    return index * 2 + 1;
+  }
+
+  private rightChild(index: number): number {
+    return index * 2 + 2;
+  }
+
+  push(val: T) {
+    this.items.push(val);
+    this.siftUp(this.size - 1);
+  }
+
+  pop(): T | undefined {
+    const res = this.peek();
+    this.swap(this.items, 0, this.size - 1);
+    this.items.pop();
+    this.siftDown(0);
+    return res;
+  }
+
+  peek(): T | undefined {
+    if (!this.isEmpty()) {
+      return this.items[0];
+    }
+  }
+
+  private siftUp(index: number): void {
+    while (
+      index > 0 &&
+      this.compare(this.items[index], this.items[this.parent(index)])
+    ) {
+      this.swap(this.items, index, this.parent(index));
+      index = this.parent(index);
+    }
+  }
+
+  private siftDown(index: number): void {
+    while (this.leftChild(index) < this.size) {
+      let pos = this.leftChild(index);
+
+      if (
+        pos + 1 < this.size &&
+        this.compare(this.items[pos + 1], this.items[pos])
+      ) {
+        pos = pos + 1; // right child
+      }
+      if (this.compare(this.items[index], this.items[pos])) {
+        break;
+      }
+      this.swap(this.items, index, pos);
+      index = pos;
+    }
+  }
+
+  private swap(arr: T[], i: number, j: number): void {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * var obj = new SmallestInfiniteSet()
+ * var param_1 = obj.popSmallest()
+ * obj.addBack(num)
+ */
+// @lc code=end
