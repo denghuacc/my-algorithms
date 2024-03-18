1bfc5fe915d03d5666b10cbb55fcffb2096c114d
Alan Deng <alanhaledc@gmail.com>
2024-03-18 12:22:16 +0800

    chore: clock 303

---
+++ leetcode/design/303.区域和检索-数组不可变.rs
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=rust
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode.cn/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (77.34%)
+ * Likes:    607
+ * Dislikes: 0
+ * Total Accepted:    249K
+ * Total Submissions: 320.5K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，处理以下类型的多个查询:
+ * 
+ * 
+ * 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+ * 
+ * 
+ * 实现 NumArray 类：
+ * 
+ * 
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和
+ * right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * 
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * 0 <= i <= j < nums.length
+ * 最多调用 10^4 次 sumRange 方法
+ * 
+ * 
+ */
+
+// @lc code=start
+struct NumArray {
+    sums: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        let n = nums.len();
+        let mut sums = vec![0; n+1];
+        for i in 1..=n {
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        return NumArray { sums };
+    }
+
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        return self.sums[right as usize + 1] - self.sums[left as usize];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray::new(nums);
+ * let ret_1: i32 = obj.sum_range(left, right);
+ */
+// @lc code=end
+
