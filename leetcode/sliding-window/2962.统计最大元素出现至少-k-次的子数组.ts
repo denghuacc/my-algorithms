bc5a0bb9b9da534370a2fa9dfdeaf5447cf0a854
denghua <denghuacc@gmail.com>
2025-04-30 10:32:01 +0800

    chore: clock 2962

---
+++ leetcode/sliding-window/2962.统计最大元素出现至少-k-次的子数组.ts
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=2962 lang=typescript
+ *
+ * [2962] 统计最大元素出现至少 K 次的子数组
+ *
+ * https://leetcode.cn/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+ *
+ * algorithms
+ * Medium (56.16%)
+ * Likes:    82
+ * Dislikes: 0
+ * Total Accepted:    28.4K
+ * Total Submissions: 47.2K
+ * Testcase Example:  '[1,3,2,3,3]\n2'
+ *
+ * 给你一个整数数组 nums 和一个 正整数 k 。
+ *
+ * 请你统计有多少满足 「 nums 中的 最大 元素」至少出现 k 次的子数组，并返回满足这一条件的子数组的数目。
+ *
+ * 子数组是数组中的一个连续元素序列。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,3,2,3,3], k = 2
+ * 输出：6
+ * 解释：包含元素 3 至少 2 次的子数组为：[1,3,2,3]、[1,3,2,3,3]、[3,2,3]、[3,2,3,3]、[2,3,3] 和
+ * [3,3] 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,4,2,1], k = 3
+ * 输出：0
+ * 解释：没有子数组包含元素 4 至少 3 次。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 10^5
+ * 1 <= nums[i] <= 10^6
+ * 1 <= k <= 10^5
+ *
+ *
+ */
+
+// @lc code=start
+function countSubarrays(nums: number[], k: number): number {
+  const maxNum = Math.max(...nums);
+  let res = 0;
+  let cnt = 0;
+  let left = 0;
+  for (const num of nums) {
+    if (num === maxNum) {
+      cnt++;
+    }
+    while (cnt >= k) {
+      if (nums[left] === maxNum) {
+        cnt--;
+      }
+      left++;
+    }
+    res += left;
+  }
+  return res;
+}
+// @lc code=end
