681ef10aa7e79604e008ee8e1e953228c30ffdf2
Alan Deng <denghuacc@gmail.com>
2025-11-21 19:56:57 +0800

    chore: clock 1930

---
+++ leetcode/hash-table/1930.长度为-3-的不同回文子序列.go
@@ -0,0 +1,112 @@
+/*
+ * @lc app=leetcode.cn id=1930 lang=golang
+ *
+ * [1930] 长度为 3 的不同回文子序列
+ *
+ * https://leetcode.cn/problems/unique-length-3-palindromic-subsequences/description/
+ *
+ * algorithms
+ * Medium (58.46%)
+ * Likes:    94
+ * Dislikes: 0
+ * Total Accepted:    23.7K
+ * Total Submissions: 37.3K
+ * Testcase Example:  '"aabca"'
+ *
+ * 给你一个字符串 s ，返回 s 中 长度为 3 的不同回文子序列 的个数。
+ *
+ * 即便存在多种方法来构建相同的子序列，但相同的子序列只计数一次。
+ *
+ * 回文 是正着读和反着读一样的字符串。
+ *
+ * 子序列 是由原字符串删除其中部分字符（也可以不删除）且不改变剩余字符之间相对顺序形成的一个新字符串。
+ *
+ *
+ * 例如，"ace" 是 "abcde" 的一个子序列。
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：s = "aabca"
+ * 输出：3
+ * 解释：长度为 3 的 3 个回文子序列分别是：
+ * - "aba" ("aabca" 的子序列)
+ * - "aaa" ("aabca" 的子序列)
+ * - "aca" ("aabca" 的子序列)
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：s = "adc"
+ * 输出：0
+ * 解释："adc" 不存在长度为 3 的回文子序列。
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：s = "bbcbaba"
+ * 输出：4
+ * 解释：长度为 3 的 4 个回文子序列分别是：
+ * - "bbb" ("bbcbaba" 的子序列)
+ * - "bcb" ("bbcbaba" 的子序列)
+ * - "bab" ("bbcbaba" 的子序列)
+ * - "aba" ("bbcbaba" 的子序列)
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 3
+ * s 仅由小写英文字母组成
+ *
+ *
+ */
+
+package leetcode
+
+import "math"
+
+// @lc code=start
+func countPalindromicSubsequence(s string) int {
+	n := len(s)
+	res := 0
+
+	firstIndex := make([]int, 26)
+	lastIndex := make([]int, 26)
+
+	for i := 0; i < 26; i++ {
+		firstIndex[i] = math.MaxInt32
+		lastIndex[i] = -1
+	}
+
+	for i := range n {
+		c := s[i] - 'a'
+		firstIndex[c] = min(firstIndex[c], i)
+		lastIndex[c] = i
+	}
+
+	for i := range 26 {
+		if firstIndex[i] < lastIndex[i] {
+			seen := make([]bool, 26)
+			for j := firstIndex[i] + 1; j < lastIndex[i]; j++ {
+				c := s[j] - 'a'
+				seen[c] = true
+			}
+			for j := 0; j < 26; j++ {
+				if seen[j] {
+					res++
+				}
+			}
+		}
+	}
+
+}
+
+// @lc code=end
